971020b9879fb2d19e1c5526df99cb9fd229ffeb
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-26 16:06:46 -0400

    Time: 54 ms (76.38%) | Memory: 51 MB (68.84%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.js
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.js
@@ -8,21 +8,22 @@
  //save index
  //lowest is weaker
 var kWeakestRows = function(mat, k) {
-    let distilledArr = []
+    let newArr = []
 
-    for (let i=0; i< mat.length; i++){
+    for (let i=0; i< mat.length; i++) {
         let sum = 0
-        for (let j=0; j< mat[i].length; j++){
-            if ( mat[i][j] === 1) sum += mat[i][j]
-        }   
-        distilledArr.push([i, sum])
+        for (let j=0; j< mat[i].length; j++) {
+            if ( mat[i][j] === 1 ) sum ++
+        }
+        newArr.push([i,sum])
     }
 
-    distilledArr.sort((a,b) => a[1] - b[1])
-
-    let answer = []
-    for (let counter =0; counter< k; counter ++){
-        answer.push(distilledArr[counter][0])
+    newArr.sort((a,b) => a[1]-b[1])
+    let ans = []
+    for (let x=0; x<k; x++ ){
+        ans.push(newArr[x][0])
     }
-    return answer 
+
+    return ans
+    
 };
