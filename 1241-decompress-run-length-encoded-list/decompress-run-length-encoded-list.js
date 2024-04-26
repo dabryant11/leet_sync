d0402790c5e4681f3e0b77396e31c64c64ac97ad
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-26 14:18:36 -0400

    Time: 62 ms (81.75%) | Memory: 52.8 MB (64.75%) - LeetSync

--- 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.js
+++ 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.js
@@ -3,19 +3,9 @@
  * @return {number[]}
  */
 var decompressRLElist = function(nums) {
-
-    //two pointer
-    let arr = []
-    let i=0
-
-    while (i<nums.length - 1) {
-        let freq = nums[i]
-        let val = nums[i + 1]
-        while (freq > 0){
-            arr.push(val)
-            freq--
-        }
-         i+=2
+   let ans = [];
+    for (let i = 0; i < nums.length; i += 2) {
+        ans.push(...Array(nums[i]).fill(nums[i + 1]));
     }
-    return arr
+    return ans;
 };
