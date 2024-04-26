ea345b5ef0867fd7f027707d2243f0d2c6329321
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-26 14:42:08 -0400

    Time: 58 ms (89.47%) | Memory: 55.1 MB (70.18%) - LeetSync

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.js
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/check-if-all-1s-are-at-least-length-k-places-away.js
@@ -4,15 +4,14 @@
  * @return {boolean}
  */
 var kLengthApart = function(nums, k) {
+    
+    let nextAllowed = 0
     ++k
-   let nextAllowed =  0
-   for (let i=0; i< nums.length; i++){
-        if ( nums[i]=== 0){
-            continue
-        } 
-        if ( i < nextAllowed) return false
+    for (let i =0; i< nums.length; i++) {
+        if (nums[i] === 0) continue
+        if (nums[i] !== 0 && i < nextAllowed) return false
+        nextAllowed = i +k
+    }
 
-        nextAllowed = i + k
-   }
-   return true
+    return true 
 };
