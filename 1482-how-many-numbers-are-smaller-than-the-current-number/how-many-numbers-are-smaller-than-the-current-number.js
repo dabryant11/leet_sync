67b6570a59284f0eff3ea77d271fb1c32c64e8ec
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-27 19:04:27 -0400

    Time: 73 ms (36.03%) | Memory: 51 MB (92.19%) - LeetSync

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+// var smallerNumbersThanCurrent = function(nums) {
+//     let sortedNums = nums.sort()
+//     let ans = []
+
+//     for (let i =1; i< nums.length; i++) {
+//         let prev = i -1
+//         let count = 0
+
+//         while (sortedNums[i] > sortedNums[prev ]&& prev > 0) {
+//             count ++
+//             prev--
+//         }
+//         ans.push(count)
+//     }
+//     return ans
+// };
+var smallerNumbersThanCurrent = function(nums) {
+    let sortedNums = nums.slice().sort((a, b) => a - b); // Make a copy and sort it
+    let ans = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+
+        // Find how many numbers are smaller than the current number in the sorted array
+        for (let j = 0; j < sortedNums.length; j++) {
+            if (sortedNums[j] < nums[i]) {
+                count++;
+            } else {
+                break; // Since the array is sorted, no need to continue checking
+            }
+        }
+
+        ans.push(count);
+    }
+
+    return ans;
+};
