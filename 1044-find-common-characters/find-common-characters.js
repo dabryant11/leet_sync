1b291f8f9990fcaacc504e202a80507ac96c9a7f
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-27 16:05:29 -0400

    Time: 55 ms (86.96%) | Memory: 53.9 MB (42.39%) - LeetSync

--- 1044-find-common-characters/find-common-characters.js
+++ 1044-find-common-characters/find-common-characters.js
@@ -5,6 +5,7 @@
 var commonChars = function(words) {
     let result = []
     for(let i = 0 ; i < words[0].length ; i++)
+
         for(let j = 1 ; j < words.length ; j++){
             if(!words[j].includes(words[0][i])) break;
             else  
@@ -12,5 +13,6 @@ var commonChars = function(words) {
             if(j == words.length - 1) result.push(words[0][i]);
         }
     return result;
+    
     
 };
