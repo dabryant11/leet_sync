541e4da8940f4389f41bc6810db22a6358a6f72e
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-27 16:24:52 -0400

    Time: 109 ms (9.52%) | Memory: 58.8 MB (69.05%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.js
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.js
@@ -14,23 +14,45 @@
     
 // };
 
+// var numEquivDominoPairs = function(dominoes) {
+//     let ans = 0;
+//     let count = {}; // Using an object to count occurrences of equivalent dominoes
+
+//     for (let i = 0; i < dominoes.length; i++) {
+//         // Sorting the dominoes to treat equivalent dominoes as identical
+//         let sortedDomino = dominoes[i].sort((a, b) => a - b);
+//         let key = sortedDomino.join('_'); // Creating a key to represent this domino
+
+//         // If this domino has been seen before, increment the count
+//         if (count[key]) {
+//             ans += count[key];
+//             count[key]++;
+//         } else {
+//             count[key] = 1;
+//         }
+//     }
+
+//     return ans;
+// };
+
+
 var numEquivDominoPairs = function(dominoes) {
-    let ans = 0;
-    let count = {}; // Using an object to count occurrences of equivalent dominoes
+    let output = 0;
+    const hashmap = new Map();
+    
+    for (let i = 0 ; i < dominoes.length; i++) {
+        const key = dominoes[i].toString();
+        const reverseKey = dominoes[i].reverse().toString();
 
-    for (let i = 0; i < dominoes.length; i++) {
-        // Sorting the dominoes to treat equivalent dominoes as identical
-        let sortedDomino = dominoes[i].sort((a, b) => a - b);
-        let key = sortedDomino.join('_'); // Creating a key to represent this domino
-
-        // If this domino has been seen before, increment the count
-        if (count[key]) {
-            ans += count[key];
-            count[key]++;
+        output += (hashmap.get(key) || 0) + (hashmap.has(reverseKey) && key !== reverseKey ? hashmap.get(reverseKey) : 0);
+        
+        
+        if (!hashmap.has(key)) {
+            hashmap.set(key, 1);
         } else {
-            count[key] = 1;
+            hashmap.set(key, hashmap.get(key) + 1);
         }
     }
-
-    return ans;
+    
+    return output;
 };
