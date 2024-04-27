8ec964ce1435bf6443566cb4df44735f846ab745
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-27 17:31:32 -0400

    Time: 87 ms (29.76%) | Memory: 59.7 MB (48.81%) - LeetSync

--- 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.js
+++ 1227-number-of-equivalent-domino-pairs/number-of-equivalent-domino-pairs.js
@@ -2,57 +2,24 @@
  * @param {number[][]} dominoes
  * @return {number}
  */
-// var numEquivDominoPairs = function(dominoes) {
-//     let ans = 0
-//     for ( let i=0; i< dominoes.length - 1; i++) {
-//         if ( dominoes[i][0] === dominoes[i + 1][0] && dominoes[i][1] === dominoes[i + 1][1] || dominoes[i][0] === dominoes[i + 1][1] && dominoes[i][1] === dominoes[i + 1][0] ) {
-//             ans ++
-//         }
-//     }
-
-//     return ans
-    
-// };
-
-// var numEquivDominoPairs = function(dominoes) {
-//     let ans = 0;
-//     let count = {}; // Using an object to count occurrences of equivalent dominoes
-
-//     for (let i = 0; i < dominoes.length; i++) {
-//         // Sorting the dominoes to treat equivalent dominoes as identical
-//         let sortedDomino = dominoes[i].sort((a, b) => a - b);
-//         let key = sortedDomino.join('_'); // Creating a key to represent this domino
-
-//         // If this domino has been seen before, increment the count
-//         if (count[key]) {
-//             ans += count[key];
-//             count[key]++;
-//         } else {
-//             count[key] = 1;
-//         }
-//     }
-
-//     return ans;
-// };
-
-
 var numEquivDominoPairs = function(dominoes) {
-    let output = 0;
-    const hashmap = new Map();
-    
-    for (let i = 0 ; i < dominoes.length; i++) {
-        const key = dominoes[i].toString();
-        const reverseKey = dominoes[i].reverse().toString();
+    let ans = 0;
+    let count = {}; // Using an object to count occurrences of equivalent dominoes
 
-        output += (hashmap.get(key) || 0) + (hashmap.has(reverseKey) && key !== reverseKey ? hashmap.get(reverseKey) : 0);
-        
-        
-        if (!hashmap.has(key)) {
-            hashmap.set(key, 1);
+    for (let i = 0; i < dominoes.length; i++) {
+        // Sorting the dominoes to treat equivalent dominoes as identical
+        let sortedDomino = dominoes[i].sort((a, b) => a - b);
+        let key = sortedDomino.join('_'); // Creating a key to represent this domino
+
+        // If this domino has been seen before, increment the count
+        if (count[key]) {
+            ans += count[key];
+            count[key]++;
         } else {
-            hashmap.set(key, hashmap.get(key) + 1);
+            count[key] = 1;
         }
     }
-    
-    return output;
+
+    return ans;
 };
+
