1641b8c9dda5c2328346faef6751495dff7bfc60
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-27 14:11:28 -0400

    Time: 199 ms (18.34%) | Memory: 79.9 MB (75.41%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.js
+++ 975-range-sum-of-bst/range-sum-of-bst.js
@@ -13,52 +13,36 @@
  * @return {number}
  */
 
-function calSum(root, low, high, sum) {
-    if (!root) return sum; // Return sum if root is null
-    if (root.val >= low && root.val <= high) {
-        sum += root.val;
-    }
-    // Update sum with values from left and right subtrees
-    sum = calSum(root.left, low, high, sum);
-    sum = calSum(root.right, low, high, sum);
-    return sum;
-}
-
-var rangeSumBST = function(root, low, high) {
-    let sum = 0;
-    return calSum(root, low, high, sum);
-};
-
 // function calSum(root, low, high, sum) {
-//     if(!root) return sum
-//     if ( root.val >= low && root.val <= high){
-//         sum += root.val
-//     }
-//     calSum(root.left, low, high, sum)
-//     calSum(root.right, low, high, sum)
-
-//     return sum
-// }
-
-
-// var rangeSumBST = function(root, low, high) {
-//     let sum = 0
-//     calSum(root, low, high, sum)
-
-// };
-
-
-// let calSum = (root, low, high) => {
-//     if (!root) return;
+//     if (!root) return sum; // Return sum if root is null
 //     if (root.val >= low && root.val <= high) {
 //         sum += root.val;
 //     }
-//     calSum(root.left, low, high);
-//     calSum(root.right, low, high);
+//     // Update sum with values from left and right subtrees
+//     sum = calSum(root.left, low, high, sum);
+//     sum = calSum(root.right, low, high, sum);
+//     return sum;
 // }
 
 // var rangeSumBST = function(root, low, high) {
-//     sum = 0; // Reset sum before each invocation
-//     calSum(root, low, high);
-//     return sum;
+//     let sum = 0;
+//     return calSum(root, low, high, sum);
 // };
+
+function calSum(root, low, high, sum) {
+    if(!root) return sum
+    if ( root.val >= low && root.val <= high){
+        sum += root.val
+    }
+    sum = calSum(root.left, low, high, sum)
+    sum = calSum(root.right, low, high, sum)
+
+    return sum
+}
+
+
+var rangeSumBST = function(root, low, high) {
+    let sum = 0
+    return calSum(root, low, high, sum)
+
+};
