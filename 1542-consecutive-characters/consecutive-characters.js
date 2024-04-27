7adc17136bbce219fe0fd41da9e967ae0075c9e4
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-27 19:53:56 -0400

    Time: 56 ms (59.45%) | Memory: 49.6 MB (23.04%) - LeetSync

---
+++ 1542-consecutive-characters/consecutive-characters.js
@@ -0,0 +1,95 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// var maxPower = function(s) {
+//     let unqCheck = {}
+//     let ans = 1
+
+//     for (let char of s) {
+//         if (!unqCheck[char]){
+//             unqCheck[char] = 1
+//         } else {
+//             unqCheck[char] ++
+//             ans = Math.max(ans, unqCheck[char])
+//         }
+//     }
+
+//     return ans
+    
+// };
+// var maxPower = function(s) {
+//     let unqCheck = {};
+//     let ans = 1; // Initialize ans with 1, as the minimum power is always 1 for a character
+
+//     for (let char of s) {
+//         if (!unqCheck[char]) {
+//             unqCheck[char] = 1;
+//         } else {
+//             unqCheck[char]++;
+//             ans = Math.max(ans, unqCheck[char]); // Update ans with the maximum count
+//         }
+//     }
+
+//     return ans;
+// };
+
+
+// var maxPower = function(s) {
+//     let unqCheck = {};
+//     let ans = 1; // Initialize ans with 1, as the minimum power is always 1 for a character
+
+//     for (let char of s) {
+//         if (unqCheck[char] === undefined) { // Check if the character is not in unqCheck
+//             unqCheck[char] = 1;
+//         } else {
+//             unqCheck[char]++;
+//             ans = Math.max(ans, unqCheck[char]); // Update ans with the maximum count
+//             delete unqCheck[char] 
+//         }
+//     }
+
+//     return ans;
+// };
+
+
+// var maxPower = function(s) {
+//     let unqCheck = {};
+//     let ans = 1; // Initialize ans with 1, as the minimum power is always 1 for a character
+
+//     for (let char of s) {
+//         if (unqCheck[char] === undefined) { // Check if the character is not in unqCheck
+//             unqCheck[char] = 1;
+//         } else {
+//             unqCheck[char]++;
+//             ans = Math.max(ans, unqCheck[char]); // Update ans with the maximum count
+//             unqCheck[char] = 1;
+//         }
+//     }
+
+//     return ans;
+// };
+
+// var maxPower = function (s) {
+//     let ans = curr = 1;
+//     for (let char of s) {
+//         if (s[char] === s[char + 1]) curr++;
+//         else {
+//             ans = Math.max(ans, curr);
+//             curr = 1;
+//         }
+//     }
+//     return ans;
+// };
+
+var maxPower = function (s) {
+    let res = curr = 1;
+    for (let i= 0; i< s.length; i++) {
+        if (s[i] === s[i + 1]) curr++;
+        else {
+            res = Math.max(res, curr);
+            curr = 1;
+        }
+    }
+    return res;
+};
