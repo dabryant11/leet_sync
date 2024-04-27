d65eb2af5ea32c13789fbcb4ac64479efe5a10e0
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-27 19:19:34 -0400

    Time: 68 ms (59.11%) | Memory: 51 MB (92.19%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.js
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.js
@@ -2,41 +2,20 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-// var smallerNumbersThanCurrent = function(nums) {
-//     let sortedNums = nums.sort()
-//     let ans = []
-
-//     for (let i =1; i< nums.length; i++) {
-//         let prev = i -1
-//         let count = 0
-
-//         while (sortedNums[i] > sortedNums[prev ]&& prev > 0) {
-//             count ++
-//             prev--
-//         }
-//         ans.push(count)
-//     }
-//     return ans
-// };
 var smallerNumbersThanCurrent = function(nums) {
-    let sortedNums = nums.slice().sort((a, b) => a - b); // Make a copy and sort it
-    let ans = [];
+    let sortedNums = nums.slice().sort((a,b) => a-b)
+    let ans = []
 
-    for (let i = 0; i < nums.length; i++) {
-        let count = 0;
-
-        // Find how many numbers are smaller than the current number in the sorted array
-        for (let j = 0; j < sortedNums.length; j++) {
-            if (sortedNums[j] < nums[i]) {
-                count++;
+    for (let i =0; i< nums.length; i++) {
+        let count = 0
+        for (let j =0; j< nums.length; j++) {
+            if (sortedNums[j] < nums[i]){
+                count ++
             } else {
                 break
             }
-                
         }
-
-        ans.push(count);
+        ans.push(count)
     }
-
-    return ans;
+    return ans
 };
