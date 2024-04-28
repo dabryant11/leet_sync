e86084f68aaab16b88ae584aeb0dc69ea173579e
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-28 19:52:03 -0400

    Time: 67 ms (11.09%) | Memory: 52.2 MB (5.47%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function(arr) {
+    let count = {0:0}
+
+
+    for ( let i=0; i< arr.length; i++) {
+        let double = arr[i] * 2
+        let half = arr[i] / 2
+        
+        if (arr[i] == 0) {
+            count[0] ++
+            if (count[0] >= 2) return true
+        }
+
+        
+        if (!count[half]) {
+            count[half] = 1 
+        }
+        if (!count[double]) {
+            count[double] = 1 
+        }
+        if (count[arr[i]] && arr[i] !== double && arr[i] !== half ) {
+            return true
+        }
+        
+    }
+    return false
+    
+};
