0005c7165be7965a1f1af64d08659456f257f5a1
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-28 19:53:47 -0400

    Time: 54 ms (64.29%) | Memory: 49 MB (79.64%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -2,31 +2,46 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var checkIfExist = function(arr) {
-    let count = {0:0}
+// var checkIfExist = function(arr) {
+//     let count = {0:0}
 
 
-    for ( let i=0; i< arr.length; i++) {
-        let double = arr[i] * 2
-        let half = arr[i] / 2
+//     for ( let i=0; i< arr.length; i++) {
+//         let double = arr[i] * 2
+//         let half = arr[i] / 2
         
-        if (arr[i] == 0) {
-            count[0] ++
-            if (count[0] > 1) return true
-        }
-
+//         if (arr[i] == 0) {
+//             count[0] ++
+//             if (count[0] > 1) return true
+//         }
         
-        if (!count[half]) {
-            count[half] = 1 
-        }
-        if (!count[double]) {
-            count[double] = 1 
-        }
-        if (count[arr[i]] && arr[i] !== double && arr[i] !== half ) {
-            return true
-        }
+//         if (!count[half]) {
+//             count[half] = 1 
+//         }
+//         if (!count[double]) {
+//             count[double] = 1 
+//         }
+//         if (count[arr[i]] && arr[i] !== double && arr[i] !== half ) {
+//             return true
+//         }
         
-    }
-    return false
+//     }
+//     return false
     
+// };
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function (arr) {
+    for (let j = 0; j < arr.length; j++) {
+        for (i = 0; i < arr.length; i++) {
+            if (arr[j] == 2 * arr[i] && i!=j) {
+                return true;
+            }
+        }
+    }
+
+    return false
 };
