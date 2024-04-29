bd1ae77c660b4e626132b7bddc08178401ec2d39
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-28 23:52:31 -0400

    Time: 51 ms (83.82%) | Memory: 50.7 MB (43.57%) - LeetSync

--- 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.js
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.js
@@ -51,19 +51,21 @@
  */
 var averageValue = function(nums) {
     let temp = []
+    let sum = 0;
+    
     nums.forEach(val =>{
-        if(val % 2 == 0 && val % 3 == 0){ // here we are segregating the elements which meet the condition listed in the question, we are using modular operator 
-            temp.push(val) // we are pushing the segregated elements to a new array
+        if(val % 2 == 0 && val % 3 == 0){ 
+            temp.push(val) 
         }
     })
-    let sum = 0;
-    for(i = 0; i < temp.length; i++){ // this loop is to get the sum of all the elements, which is stored in sum
+
+    for(i = 0; i < temp.length; i++){ 
         sum += temp[i]
     }
-    sum = sum / temp.length; // we are using temp.length here coz we dont know how many elements will meet the codition and to find the sum..
-    if(sum > 0 ){ // this if statement is used in case none of the element meets the condition
-	
-        return Math.floor(sum) // here we are using Math.floor to round down the no. 
-		
-    } return 0 // in case none of the element meets the condition this line will be executed
+    sum = sum / temp.length; 
+    if(sum > 0 ){
+        return Math.floor(sum) 	
+    } 
+
+    return 0
 };
