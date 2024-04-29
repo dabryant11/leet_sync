2ca61a5c13b68ac52cbb67f5f51516099ff82583
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-29 12:43:42 -0400

    Time: 61 ms (25.80%) | Memory: 51.2 MB (11.59%) - LeetSync

--- 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.js
+++ 2421-maximum-number-of-pairs-in-array/maximum-number-of-pairs-in-array.js
@@ -4,25 +4,25 @@
  */
 var numberOfPairs = function(nums) {
     let count = {}
-    let pair = 0 
-    let single = 0 
     let ans = []
+    let pair = 0
+    let single = 0
 
     for ( let num of nums) {
         if ( !count[num]) count[num] = 1
         else {
             count[num] ++
-            if ( count[num] == 2) {
-                count[num] = 0
+            if ( count[num] == 2){
                 pair ++
+                count[num] = 0
             }
         }
     }
-    
-    for (let key of Object.keys(count)) {
+
+    for (let key of Object.keys(count)){
         if ( count[key] == 1) single ++
     }
+
     ans.push(pair, single)
-    
     return ans
 };
