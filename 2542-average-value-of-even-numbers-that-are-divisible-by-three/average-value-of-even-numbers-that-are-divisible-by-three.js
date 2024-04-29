0b028152a92f54d3fcbc7561de9de5a08af116e0
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-29 00:56:36 -0400

    Time: 58 ms (46.06%) | Memory: 50.7 MB (39.42%) - LeetSync

--- 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.js
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.js
@@ -8,42 +8,14 @@ var averageValue = function(nums) {
     for (let num of nums) {
         if(num % 2 == 0 && num % 3 == 0) temp.push(num) 
     }
-
-
     for ( let num of temp) {
         sum += num
     }
 
-    // sum = sum / temp.length; 
     if(temp.length ){
+        // return sum / temp.length
         return Math.floor(sum / temp.length) 	
     } 
 
-// if (temp.length) ans = ans / temp.length
-
-//     return ans
-
-
-
     return 0
 };
-
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-// var averageValue = function(nums) {
-//     let temp = []
-//     let ans= 0
-//     for ( let num of nums) {
-//         if ( num %2==0 && num% 3== 0) temp.push(num)
-//     }
-
-//     for ( let num of temp) {
-//         ans += num
-//     }
-
-//     if (temp.length) ans = ans / temp.length
-
-//     return ans
-// };
