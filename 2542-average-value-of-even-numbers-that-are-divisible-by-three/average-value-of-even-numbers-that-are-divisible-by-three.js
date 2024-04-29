ccd71b6790907f2cbd5c21e3a6e5b3a2f7d00ade
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-29 00:24:23 -0400

    Time: 66 ms (14.11%) | Memory: 50.8 MB (39.42%) - LeetSync

--- 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.js
+++ 2542-average-value-of-even-numbers-that-are-divisible-by-three/average-value-of-even-numbers-that-are-divisible-by-three.js
@@ -1,50 +1,3 @@
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-// var averageValue = function(nums) {
-//     //iterate nums
-//     //even num && div by 3
-//     //ans  - add num to ans
-//     // key is ans 
-//     //val is num of nums
-//     // return ans 
-//     let count = {}
-//     let ans = 0
-//     let numOfNums = 0
-//     let keys = Object.keys(count);
-//     for ( let i =0; i< nums.length; i++) {
-//         if ( nums[i]%2 == 0 && nums[i] % 3 == 0) {
-//             numOfNums ++
-//             ans += nums[i]
-//             count[ans] = numOfNums
-//         }
-//     }
-//     ans = keys[0]/numOfNums
-//     return ans
-// };
-
-
-/**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-
-// var averageValue = function(nums) {
-//     let sum = 0;
-//     let count = 0;
-
-//     for (let i = 0; i < nums.length; i++) {
-//         if (nums[i] % 2 === 0 && nums[i] % 3 === 0) {
-//             sum += nums[i];
-//             count++;
-//         }
-//     }
-
-//     return count > 0 ? sum / count : 0;
-// };
-
-
 /**
  * @param {number[]} nums
  * @return {number}
@@ -52,7 +5,7 @@
 var averageValue = function(nums) {
     let temp = []
     let sum = 0;
-    
+
     nums.forEach(val =>{
         if(val % 2 == 0 && val % 3 == 0){ 
             temp.push(val) 
