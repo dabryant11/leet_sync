8f41cee0dc33d163746b3b6b053756e70ffa6cf3
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-29 13:31:41 -0400

    Time: 45 ms (93.09%) | Memory: 49.2 MB (64.00%) - LeetSync

--- 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.js
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.js
@@ -1,31 +1,44 @@
+// /**
+//  * @param {number[]} nums
+//  * @param {number} original
+//  * @return {number}
+//  */
+// // var findFinalValue = function(nums, original) {
+// //     //iterate
+// //     // num og check
+// //     // 2 * og  = og
+// //     let newOriginal = original
+// //     for ( let num of nums) {
+// //         if ( num == newOriginal) {
+// //             newOriginal *= 2
+// //         }
+// //     }
+// //     return newOriginal 
+    
+// // };
+
+// var findFinalValue = function(nums, original) {
+//     let newOriginal = original;
+//     nums.sort((a,b) => a-b)
+//     for (let num of nums) {
+// console.log("num", num)
+// console.log("newOriginal", newOriginal)
+//         if (num === newOriginal) {
+//             newOriginal *= 2;
+//         }
+//     }
+//     return newOriginal;
+// };
+
+
 /**
  * @param {number[]} nums
  * @param {number} original
  * @return {number}
  */
-// var findFinalValue = function(nums, original) {
-//     //iterate
-//     // num og check
-//     // 2 * og  = og
-//     let newOriginal = original
-//     for ( let num of nums) {
-//         if ( num == newOriginal) {
-//             newOriginal *= 2
-//         }
-//     }
-//     return newOriginal 
-    
-// };
-
 var findFinalValue = function(nums, original) {
-    let newOriginal = original;
-    nums.sort((a,b) => a-b)
-    for (let num of nums) {
-console.log("num", num)
-console.log("newOriginal", newOriginal)
-        if (num === newOriginal) {
-            newOriginal *= 2;
-        }
+    while (nums.includes(original)) {
+        original *= 2;
     }
-    return newOriginal;
+    return original;
 };
