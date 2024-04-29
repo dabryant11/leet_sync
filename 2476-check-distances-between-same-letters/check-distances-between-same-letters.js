d2f4bffd7f0e48a4f7b3a46e0942af990ef0280e
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-29 13:47:33 -0400

    Time: 57 ms (72.22%) | Memory: 49.8 MB (71.11%) - LeetSync

---
+++ 2476-check-distances-between-same-letters/check-distances-between-same-letters.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {number[]} distance
+ * @return {boolean}
+ */
+// var checkDistances = function(s, distance) {
+//     //iterate
+//     // add to map - key letter val idx
+//     //map check - abs.(curr idx - keyed val)
+
+// };
+
+// Time complexity: O(n), where n is length of input string.
+// Space complexity: O(1)
+
+var checkDistances = function(s, distance) {
+    const firstIndex = Array(26).fill(-1);
+	
+	for(let index = 0; index < s.length; index++) {
+		const charCode = s[index].charCodeAt(0) - 'a'.charCodeAt(0);
+		
+		if(firstIndex[charCode] !== -1)
+			if(index - firstIndex[charCode] - 1 !== distance[charCode])
+				return false;
+		
+		firstIndex[charCode] = index;
+	}
+	
+	return true;
+};
