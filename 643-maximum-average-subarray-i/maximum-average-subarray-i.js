8872f6e67fe8d8fc1689183f6640134da137a16d
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-30 16:56:08 -0400

    Time: 82 ms (57.88%) | Memory: 63.1 MB (22.47%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.js
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.js
@@ -4,19 +4,18 @@
  * @return {number}
  */
 var findMaxAverage = function(nums, k) {
+    let maxSum = 0
     let currSum = 0
-    
 
-    for ( let i =0; i< k; i++) {
+    for (let i =0; i< k; i++){
         currSum += nums[i]
     }
-    
-    let maxSum = currSum
+    maxSum = currSum
 
-    for ( let i=k;i<nums.length;i++){
+    for ( let i=k; i<nums.length; i++) {
         currSum += nums[i] - nums[i - k]
         maxSum = Math.max(maxSum, currSum)
     }
-
     return maxSum / k
+    
 };
