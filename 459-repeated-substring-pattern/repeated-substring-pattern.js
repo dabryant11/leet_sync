1085cb6695e1ea1cfbb04cd1999c9ce5c4f4b2de
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-30 15:22:53 -0400

    Time: 50 ms (93.27%) | Memory: 54.6 MB (61.39%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.js
+++ 459-repeated-substring-pattern/repeated-substring-pattern.js
@@ -5,7 +5,9 @@
 var repeatedSubstringPattern = function(s) {
     // count
     // compare curr win with key
-       const doubled = s + s;
+    const doubled = s + s;
     const sub = doubled.slice(1, -1);
+    console.log("sub", sub)
+    console.log("doubled", doubled)
     return sub.includes(s);
 };
