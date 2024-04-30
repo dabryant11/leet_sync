8562f17bf5d73e8a8872cd08706a6fee07643a9a
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-30 12:52:43 -0400

    Time: 87 ms (5.47%) | Memory: 51.5 MB (70.31%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.js
+++ 496-next-greater-element-i/next-greater-element-i.js
@@ -3,33 +3,40 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var nextGreaterElement = function(nums1, nums2) {
-    let ans = [];
-    let map = {};
 
-    // Create a map of numbers to their indices in nums2
-    for (let i = 0; i < nums2.length; i++) {
-        map[nums2[i]] = i;
-    }
-
-    // Iterate over nums1
-    for (let i = 0; i < nums1.length; i++) {
-        let found = false;
-        let idx = map[nums1[i]] + 1; // Index of the next element in nums2
-        // Check if there is a next greater element
-        while (idx < nums2.length) {
-            if (nums2[idx] > nums1[i]) {
-                ans.push(nums2[idx]);
-                found = true;
-                break;
-            }
-            idx++;
+    var nextGreaterElement = function(nums1, nums2) {
+    const map = {}
+    const output = []
+	
+	// loop on the nums2 and map its element as keys and it index as values
+    for (let k = 0; k < nums2.length; k++) {
+	
+	    // if current iteration element is their in map then continue
+        if (map.hasOwnProperty(nums2[k])) {
+            continue
+			
+		// else assign key as element and index as value
+        } else {
+            map[nums2[k]] = k
+        }    
+    } 
+	
+	// nested for loop to check the first greater value with the current value
+    for (let i = 0; i < nums1.length; i++) { 
+	
+		// get index of current iteration element of nums1 array i.e {nums1[i]} from the map
+        let nums1EleIndexInNums2Array = map[nums1[i]]
+		
+        let currMaxVal = -1
+		
+		// for loop to check all the elements in nums2 array 
+        for (let j = nums1EleIndexInNums2Array; j < nums2.length; j++) {
+            if (nums1[i] < nums2[j]) {
+                currMaxVal = nums2[j]
+                break 
+            }    
         }
-        // If no greater element found, push -1
-        if (!found) {
-            ans.push(-1);
-        }
-    }
-
-    return ans;
+        output.push(currMaxVal)
+    } 
+    return output
 };
