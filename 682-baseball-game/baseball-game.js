c2910ec0d09c58da73d0dc0007a5735f8d4b03e0
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-30 16:15:12 -0400

    Time: 52 ms (75.62%) | Memory: 48.9 MB (88.94%) - LeetSync

--- 682-baseball-game/baseball-game.js
+++ 682-baseball-game/baseball-game.js
@@ -3,22 +3,27 @@
  * @return {number}
  */
 var calPoints = function(operations) {
-    let record = [];
-    for (let i = 0; i < operations.length; i++) {
-        if (operations[i] === "+") {
-            record.push(record[record.length - 2] + record[record.length - 1]);
-        } else if (operations[i] === "D") {
-            record.push(record[record.length - 1] * 2);
-        } else if (operations[i] === "C") {
-            record.pop(); // Corrected to remove the last element
-        } else {
-            record.push(parseInt(operations[i])); // Convert string to number
+ 
+    let record = []
+    for ( let i =0; i < operations.length; i++){
+        if (operations[i] === "+"){
+            record.push(record[record.length -2] + record[record.length -1])
+        }
+        else if (operations[i] === "D"){
+            record.push(record[record.length -1] * 2)
+        }
+        else if (operations[i] === "C"){
+            record.pop()
+        }
+        else {
+            record.push(parseInt(operations[i]))
         }
     }
 
-    let ans = 0;
-    for (let num of record) {
-        ans += num;
+    let ans = 0
+    for ( let num of record){
+        ans += num
     }
-    return ans;
+    return ans
+    
 };
