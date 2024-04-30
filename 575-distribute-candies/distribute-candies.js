3cfd86c25d7f6d154ddd3969877ebbbdee98549a
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-30 11:48:30 -0400

    Time: 229 ms (15.90%) | Memory: 66.9 MB (11.03%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function(candyType) {
+    //iterate
+    //map check
+    //add to count
+    // min of count or n/2
+    let count = {}
+    let unq = 0
+    let maxNum = candyType.length  /2
+
+    for ( let num of candyType) {
+        if (!count[num]) {
+            count[num] = 1
+            unq++
+        }
+    }
+console.log("unq", unq)
+console.log("maxNum", maxNum)
+    return unq > maxNum ? maxNum : unq
+};
