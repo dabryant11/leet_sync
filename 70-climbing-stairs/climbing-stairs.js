b3f0e0660d6ad0565bc9b0f77463968673f5d2da
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-30 12:35:00 -0400

    Time: 43 ms (90.77%) | Memory: 48.9 MB (29.38%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -3,13 +3,12 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    const dp = new Array(n + 1).fill(0);
-    dp[0] = 1;
-    dp[1] = 1;
+    let dp = new Array(n+1).fill(0)
+    dp[0] = 1
+    dp[1] = 1
 
-    for (let i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+    for (let i =2; i<=n; i++){
+        dp[i] = dp[i-1] + dp[i-2]
     }
-
-    return dp[n];    
+    return dp[n]
 };
