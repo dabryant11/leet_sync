687171a75152b763b31ea85d57bb4de574551a0f
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-30 12:09:49 -0400

    Time: 57 ms (63.62%) | Memory: 51.1 MB (78.48%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.js
+++ 404-sum-of-left-leaves/sum-of-left-leaves.js
@@ -12,11 +12,12 @@
  */
 var sumOfLeftLeaves = function(root) {
     if ( !root) return 0
-    return helper(root.left, true)+  helper(root.right, false)
+    return helper(root.left, true) + helper(root.right, false)
 };
     
-function helper(root, isLeft){
-    if ( !root) return 0
-    if ( !root.left && !root.right && isLeft) return root.val
-    return helper(root.left, true) + helper(root.right, false)
-}    
+    function helper(root, isLeft) {
+        if ( !root) return 0
+        if ( !root.left && !root.right && isLeft) return root.val
+        return helper(root.left, true) + helper(root.right, false)
+    }
+    
