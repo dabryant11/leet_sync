2ed078b80b74c7f97a808a64a860e5d7d8242698
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-04-30 23:14:03 -0400

    Time: 105 ms (29.28%) | Memory: 55.1 MB (41.86%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,49 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// var lengthOfLongestSubstring = function(s) {
+//     //sliding window
+//     // iterate
+//     // curr, max
+//     // not same curr ++
+//     // same math max curr max
+//           // curr 0
+
+//     let currSum = 0
+//     let maxSum = 0
+//     let count = {}
+
+//     for (let char of s){
+//         if (!count[char]) currSum ++
+//         else {
+//             maxSum = Math.max(maxSum, currSum)
+//             count = {}
+//         }
+//     }
+
+//     return maxSum
+// };
+
+
+
+
+var lengthOfLongestSubstring = function(s) {
+    let currSum = 0;
+    let maxSum = 0;
+    let count = {};
+    let start = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if (count[char] !== undefined && count[char] >= start) {
+            start = count[char] + 1;
+            currSum = i - start;
+        }
+        currSum++;
+        count[char] = i;
+        maxSum = Math.max(maxSum, currSum);
+    }
+
+    return maxSum;
+};
