13380d972c87ca199c22e63e8221582a6f9da187
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-01 20:16:49 -0400

    Time: 83 ms (30.99%) | Memory: 53.5 MB (45.94%) - LeetSync

--- 258-add-digits/add-digits.js
+++ 258-add-digits/add-digits.js
@@ -1,44 +1,17 @@
-// /**
-//  * @param {number} num
-//  * @return {number}
-//  */
-// var addDigits = function(num) {
-//     // num to str
-//     //newSum
-//     //iterate
-//     // add to newSum
-//     // newSum to s
-//     //check newNum len
-//     //newNum to int
-
-//     newNum = toString(num)
-//     let currNum = 0
-//     for ( let i=0; i< newNum.length;i++){
-//         currNum += newNum[i]
-        
-//         parseInt(currSum)
-//         if (currSum.length == 1 && !i+1){
-//             return currSum
-//         }
-//     }
-// };
-
-
-
 /**
  * @param {number} num
  * @return {number}
  */
 var addDigits = function(num) {
-    let newNum = num.toString(); // Convert num to string
-    let currSum = 0; // Initialize currSum to accumulate the sum of digits
-    for (let i = 0; i < newNum.length; i++) {
-        currSum += parseInt(newNum[i]); // Convert each character to number and add to currSum
+    let newNum = num.toString()
+    let currNum = 0
+
+    for (let i=0; i<newNum.length; i++){
+        currNum += parseInt(newNum[i])
     }
-    // If the sum has more than one digit, recursively call addDigits
-    if (currSum >= 10) {
-        return addDigits(currSum);
-    } else {
-        return currSum; // Otherwise, return the sum
-    }
+
+    if (currNum >= 10){
+        return addDigits(currNum)
+    }else return currNum
+
 };
