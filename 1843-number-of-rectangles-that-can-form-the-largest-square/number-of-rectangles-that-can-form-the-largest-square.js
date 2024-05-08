5432ef1b4e588c86b56981fb7c927e633e958876
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-08 14:55:59 -0400

    Time: 64 ms (41.49%) | Memory: 52.2 MB (40.43%) - LeetSync

---
+++ 1843-number-of-rectangles-that-can-form-the-largest-square/number-of-rectangles-that-can-form-the-largest-square.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} rectangles
+ * @return {number}
+ */
+var countGoodRectangles = function(rectangles) {
+    // iterate
+    // find min then push to arr
+    let counter = 1
+    let maxSq = -1
+    for ( let sq of rectangles ) {
+        let currSq = Math.min(sq[0], sq[1])
+        
+        if (currSq > maxSq) {
+            maxSq = currSq
+            counter = 1 
+        }
+        else if (currSq == maxSq) counter ++
+        
+    }
+    return counter
+};
