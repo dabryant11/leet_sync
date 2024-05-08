7b6903892e2a5b7cf886ee50ad5f0d8f23aab24d
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-08 14:25:24 -0400

    Time: 70 ms (41.49%) | Memory: 52.2 MB (52.13%) - LeetSync

---
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var separateDigits = function(nums) {
+    let ans = []
+    let string = ''
+    for (let i=0; i<nums.length;i++){
+        string+= nums[i]
+    }
+console.log("string", string)
+    for (let i=0;i<string.length;i++){
+        ans.push(string[i])
+    }
+    return ans
+};
