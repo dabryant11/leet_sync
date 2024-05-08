9ed2a69d5e28d18ce083b18d7e03cd7fc32c76f3
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-08 18:19:07 -0400

    Time: 142 ms (85.98%) | Memory: 58.3 MB (90.39%) - LeetSync

--- 326-power-of-three/power-of-three.js
+++ 326-power-of-three/power-of-three.js
@@ -3,12 +3,13 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    
-    while ( n > 3){
-        n = n / 3
-    }
+while(n>1) n=n/3
+return n==1    
+    // while ( n > 3){
+    //     n = n / 3
+    // }
 
-    if ( n <= 0  ) return false 
-    if ( n % 3 === 0 || n == 1 ) return true
-    else return false
+    // if ( n <= 0  ) return false 
+    // if ( n % 3 === 0 || n == 1 ) return true
+    // else return false
 };
