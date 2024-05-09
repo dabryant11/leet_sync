6cce4b988df6d9093c131266219b11eed1b1893a
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-09 17:26:01 -0400

    Time: 78 ms (37.06%) | Memory: 53.1 MB (56.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -4,10 +4,8 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    // Convert strings to arrays, sort them, and convert them back to strings
-    let sortedS = s.split("").sort().join("");
-    let sortedT = t.split("").sort().join("");
+    let sortedS = s.split("").sort().join("")
+    let sortedT = t.split("").sort().join("")
     
-    // Compare the sorted strings
-    return sortedS === sortedT;
+    return sortedT === sortedS
 };
