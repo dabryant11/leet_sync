3f34e55ce680a045eaa50334c26cb52fa8dab2d9
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-09 18:29:00 -0400

    Time: 63 ms (25.18%) | Memory: 51.6 MB (15.32%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
@@ -3,22 +3,21 @@
  * @return {string}
  */
 var removeOuterParentheses = function(s) {
-    let stack = [];
-    let ans = "";
-    
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === "(") {
-            if (stack.length > 0) {
-                ans += "("; // Add '(' to the result if it's not the outermost '('
-            }
-            stack.push("("); // Push '(' to the stack
-        } else if (s[i] === ")") {
-            stack.pop(); // Pop '(' from the stack
-            if (stack.length > 0) {
-                ans += ")"; // Add ')' to the result if it's not the outermost ')'
-            }
+    let stack = []
+    let pointer = 0
+    let ans = ""
+
+    while (pointer < s.length){
+        if (s[pointer] === "("){
+            if ( stack.length > 0) ans += "("
+        
+        stack.push("(")
+        }else if (s[pointer] === ")"){
+            stack.pop()
+            if ( stack.length > 0) ans += ")"
         }
+        pointer ++
     }
-    
-    return ans;
+
+    return ans
 };
