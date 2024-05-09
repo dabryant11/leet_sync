87beb0cfc48a529f0f900d35c3c32305e00262a7
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-08 23:43:02 -0400

    Time: 57 ms (82.85%) | Memory: 51.5 MB (78.16%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -2,35 +2,17 @@
  * @param {string} s
  * @return {boolean}
  */
-// var isPalindrome = function(s) {
-//     let left = 0
-//     let right = s.length -1
-//     for (let char of s){
-//         if (s[char] !== /^[a-zA-Z0-9 ]+$/) s.splice(char,1)
-//     }
-
-//     while ( left < right) {
-//         if (s[left].toLowerCase() !== s[right].toLowerCase()) return false
-//         left ++
-//         right --
-//     }
-//     return true 
-// };
-
-
 var isPalindrome = function(s) {
-    // Remove non-alphanumeric characters and convert to lowercase
-    const alphanumeric = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let alphanumeric = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
-    let left = 0;
-    let right = alphanumeric.length - 1;
+    
+    let left = 0
+    let right = alphanumeric.length -1
 
-    while (left < right) {
-        if (alphanumeric[left] !== alphanumeric[right]) {
-            return false;
-        }
-        left++;
-        right--;
+    while ( left < right) {
+        if (alphanumeric[left] !== alphanumeric[right]) return false
+        left ++
+        right --
     }
-    return true;
+    return true 
 };
