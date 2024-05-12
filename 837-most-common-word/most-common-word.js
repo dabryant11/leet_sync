1542da1c9cd8db65ac85a23bcd6592fa343bb777
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-11 23:48:44 -0400

    Time: 71 ms (16.90%) | Memory: 50.8 MB (79.58%) - LeetSync

---
+++ 837-most-common-word/most-common-word.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function(paragraph, banned) {
+    let count = {};
+    // Convert paragraph to lowercase and split into an array of words
+    let parArr = paragraph.toLowerCase().split(/\W+/);
+    
+    // Count occurrences of each word
+    for (let i = 0; i < parArr.length; i++) {
+        let word = parArr[i];
+        if (!banned.includes(word) && word.length > 0) {
+            count[word] = (count[word] || 0) + 1;
+        }
+    }
+console.log("count", count)
+    // Find the most common word
+    let mostCommon = '';
+    let maxCount = 0;
+    for (let word in count) {
+        if (count[word] > maxCount) {
+            mostCommon = word;
+            maxCount = count[word];
+        }
+    }
+
+    return mostCommon;
+};
+
+
