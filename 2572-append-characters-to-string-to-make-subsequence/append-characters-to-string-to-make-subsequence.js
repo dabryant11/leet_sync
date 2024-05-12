6ea2f760b0380a26fb2f5d7bc8bffd74dfdd94aa
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-11 23:20:39 -0400

    Time: 53 ms (84.04%) | Memory: 52.9 MB (40.43%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.js
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.js
@@ -4,7 +4,8 @@
  * @return {number}
  */
 var appendCharacters = function(s, t) {
-      let j =0, len = t.length
+    let j =0
+    let len = t.length
     for(let i=0;i<s.length;i++){
         s[i]===t[j] && j++
         if(len===j) return 0
