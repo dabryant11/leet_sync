65bd2ded0d7868321fb3d930dbfebf29b0182b76
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-11 22:20:29 -0400

    Time: 158 ms (78.72%) | Memory: 61.5 MB (97.87%) - LeetSync

--- 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.js
+++ 2497-maximum-matching-of-players-with-trainers/maximum-matching-of-players-with-trainers.js
@@ -4,20 +4,20 @@
  * @return {number}
  */
 var matchPlayersAndTrainers = function(players, trainers) {
- players.sort((a,b) => b-a);
-    trainers.sort((a,b) => b-a);
-    let playersPointer = 0;
-    let trainersPointer = 0;
-    let ans = 0;
+    players.sort((a,b) => b-a)
+    trainers.sort((a,b) => b-a)
+    let playersPointer = 0
+    let trainersPointer = 0
+    let ans = 0
     
     while (trainersPointer < trainers.length && playersPointer < players.length){
         if ( trainers[trainersPointer] >= players[playersPointer]) {
-            trainersPointer ++;
-            playersPointer ++;
-            ans ++;
+            trainersPointer ++
+            playersPointer ++
+            ans ++
         } else {
-            playersPointer ++;
+            playersPointer ++
         }
     }
-    return ans;
+    return  ans
 };
