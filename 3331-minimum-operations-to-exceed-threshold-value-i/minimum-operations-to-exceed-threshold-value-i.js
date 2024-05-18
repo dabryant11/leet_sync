244a366a9ddc993ca6203786d825c51804b230e6
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-18 15:33:09 -0400

    Time: 49 ms (96.41%) | Memory: 51.2 MB (42.40%) - LeetSync

--- 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.js
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.js
@@ -4,11 +4,9 @@
  * @return {number}
  */
 var minOperations = function(nums, k) {
-    nums.sort((a,b) => a-b)
     let ans = 0
     for (let i=0;i<nums.length;i++){
         if(nums[i] < k) ans ++
-        if(nums[i] == k) break
     }
     return ans
 };
