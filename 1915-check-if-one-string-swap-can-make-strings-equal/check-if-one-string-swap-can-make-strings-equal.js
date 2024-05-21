f45c53edd73be3ad80d58da7ab33fb1cc1362fa8
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-20 19:37:18 -0700

    Time: 49 ms (73.63%) | Memory: 48.9 MB (56.04%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.js
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.js
@@ -4,31 +4,16 @@
  * @return {boolean}
  */
 var areAlmostEqual = function(s1, s2) {
-    // If the strings are already equal, return true
-    if (s1 === s2) return true;
+    let idxDiff = []
 
-    // If the lengths are not equal, return false
-    if (s1.length !== s2.length) return false;
-    
-    // Array to store indices where characters differ
-    let diffIndices = [];
-
-    // Loop through the strings and collect indices of mismatched characters
-    for (let i = 0; i < s1.length; i++) {
-        if (s1[i] !== s2[i]) {
-            diffIndices.push(i);
+    for (let i=0;i<s1.length;i++){
+        if (s1[i] !== s2[i]){
+            idxDiff.push(i)
         }
     }
 
-    // If there are no mismatches, strings are already equal
-    if (diffIndices.length === 0) return true;
-    
-    // If there are exactly two mismatches, check if swapping makes the strings equal
-    if (diffIndices.length === 2) {
-        let [i, j] = diffIndices;
-        return s1[i] === s2[j] && s1[j] === s2[i];
-    }
-
-    // More than two mismatches means they can't be made equal with one swap
-    return false;
+    if (s2 === s1) return true
+    if (idxDiff.length > 2) return false
+    let [i,j] = idxDiff
+    return s1[i] === s2[j] && s1[j] === s2[i]
 };
