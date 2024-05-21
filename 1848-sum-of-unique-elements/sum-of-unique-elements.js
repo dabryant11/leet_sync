8234b388071282d2e243714d6c0d21aa778aa0e5
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-20 20:02:36 -0700

    Time: 59 ms (29.88%) | Memory: 48.9 MB (71.67%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.js
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.js
@@ -3,19 +3,17 @@
  * @return {number}
  */
 var sumOfUnique = function(nums) {
-    let count = {};
-    let sum = 0;
-
-    // Count the occurrences of each number in nums
-    for (let i = 0; i < nums.length; i++) {
-        if (!count[nums[i]]) count[nums[i]] = 0;
-        count[nums[i]]++;
+    let count = {}
+    let sum = 0
+    for (let i=0; i<nums.length;i++){
+        if (!count[nums[i]]) count[nums[i]] = 0
+        count[nums[i]] ++
     }
 
-    // Sum the numbers that occur exactly once
-    for (let key in count) {
-        if (count[key] === 1) sum += parseInt(key);
-    }
+    for ( let key in count){
+        if (count[key] === 1) sum += parseInt(key)
+        
 
-    return sum;
+    }
+    return sum
 };
