5432c0d893f9cebecb1f55900983e07bc998f1f1
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-20 21:08:05 -0700

    Time: 78 ms (17.61%) | Memory: 51.7 MB (78.17%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.js
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.js
@@ -3,18 +3,24 @@
  * @return {boolean}
  */
 var makeEqual = function(words) {
-    let count = {}
-    if (words.length === 1) return true
+    let count = {};
+    let totalWords = words.length;
 
-    for (let i=0; i<words.length;i++){
-        for (let j=0; j<words[i].length;j++){
-            if (!count[words[i][j]]) count[words[i][j]] = 0
-            count[words[i][j]] ++
+    if (totalWords === 1) return true;
+
+    // Count the occurrences of each character in all words
+    for (let i = 0; i < words.length; i++) {
+        for (let j = 0; j < words[i].length; j++) {
+            let char = words[i][j];
+            if (!count[char]) count[char] = 0;
+            count[char]++;
         }
     }
 
-    for(let key in count){
-        if (count[key] % words.length !== 0) return false
+    // Check if each character count is divisible by the number of words
+    for (let key in count) {
+        if (count[key] % totalWords !== 0) return false;
     }
-    return true
+
+    return true;
 };
