b16dbd36889df2cf7363aa74a77fbf854772fcae
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-20 22:25:56 -0700

    Time: 58 ms (43.85%) | Memory: 51.5 MB (23.77%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.js
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.js
@@ -3,12 +3,34 @@
  * @return {number}
  */
 var maximumCount = function(nums) {
-    let posCount = 0
-    let negCount = 0
-    for (let i=0;i<nums.length;i++){
-        if (nums[i]< 0) negCount ++
-        if (nums[i]> 0) posCount ++
+    const n = nums.length;
+    if (nums[0] > 0 || nums[n - 1] < 0) return n;
+    let left = 0;
+    let right = n - 1;
+    while (left < right) {
+        const mid = left + Math.floor((right - left) / 2);
+        if (nums[mid] < 0) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
     }
-    let ans = Math.max(posCount,negCount)
-    return ans
+    // now left pointer points the most left position that nums[left] >= 0
+    if (nums[left] > 0) {
+        return Math.max(left, n - left);
+    }
+    // if nums[left] is equals to 0, then we need to find the zero on the most right position
+    const neg = left;
+    right = n - 1;
+    while (left < right) {
+        const mid = left + Math.floor((right - left) / 2);
+        if (nums[mid] > 0) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    // defind the number of pos by checking nums[left]
+    const pos = nums[left] === 0 ? n - left - 1 : n - left;
+    return Math.max(pos, neg);
 };
