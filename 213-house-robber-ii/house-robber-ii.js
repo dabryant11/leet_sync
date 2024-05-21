5db8d565bff7672be0a8e42fa37c595e1074eae6
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-21 12:12:57 -0700

    Time: 42 ms (94.68%) | Memory: 48.9 MB (48.31%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    const len = nums.length;
+    if (len < 4) return Math.max(...nums);
+    
+    function robberHelper(start, end) {
+        let prev=0, beforePrev=0;
+        for (let i=start;i<end;i++) {
+            let tmp = prev;
+            prev = Math.max(nums[i] + beforePrev, prev);
+            beforePrev= tmp;
+        }
+        return prev;
+    }
+    return Math.max(robberHelper(0, len-1), robberHelper(1, len));
+};
