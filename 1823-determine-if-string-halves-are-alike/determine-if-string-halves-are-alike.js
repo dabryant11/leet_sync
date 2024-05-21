9fc5cbd6cbbe650ef8fa201bcfe40241d90737df
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-20 20:21:10 -0700

    Time: 51 ms (82.71%) | Memory: 49.1 MB (88.76%) - LeetSync

---
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var halvesAreAlike = function(s) {
+    let firstHalf = true
+    let firsthVow = 0
+    let sechVow = 0
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    for (let i=0;i<s.length;i++){
+        if (i >= s.length/2) firstHalf = false
+        if (vowels.includes(s[i]) && firstHalf) firsthVow ++
+        if (vowels.includes(s[i]) && !firstHalf) sechVow ++
+
+    }
+
+    return firsthVow === sechVow
+};
