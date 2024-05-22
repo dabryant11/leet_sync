5077f6cbe404731e1f9028c17fd89432dc722f69
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-05-21 19:45:47 -0700

    Time: 182 ms (92.16%) | Memory: 81.1 MB (22.78%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.js
+++ 435-non-overlapping-intervals/non-overlapping-intervals.js
@@ -3,13 +3,15 @@
  * @return {number}
  */
 var eraseOverlapIntervals = function(intervals) {
-  // sort by earliest finish time
-    intervals.sort((a, b) => a[1] - b[1]);
-    let prev = intervals[0], remove = 0;
-    
-    for(let i = 1; i < intervals.length; i++) {
-        if(intervals[i][0] < prev[1]) remove++;
-        else prev = intervals[i];
+    intervals.sort((a,b) => a[1] - b[1])
+    let counter = 0
+    let prev = intervals[0]
+    for (let i=1;i<intervals.length;i++){
+        let curr = intervals[i]
+        let start = 0
+        let end = 1
+        if (curr[start] < prev[end]) counter ++
+        else prev = intervals[i]
     }
-    return remove;
+    return counter 
 };
