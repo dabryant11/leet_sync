c5e151b4def709a4eb3e699bfa6fd9b7d251752f
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-06-04 12:23:24 -0700

    Time: 57 ms (37.63%) | Memory: 48.6 MB (94.39%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
@@ -3,10 +3,5 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-    let count = {}
-    for (let i=0;i<sentence.length;i++){
-        if (!count[sentence[i]]) count[sentence[i]] = 1
-    }
-
-    return Object.keys(count).length === 26
+    return new Set(sentence.split('')).size === 26 
 };
