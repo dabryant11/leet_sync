1e677bcc142bde924bc866be54e15ea80d142665
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-06-04 15:55:14 -0700

    Time: 128 ms (20.17%) | Memory: 54.6 MB (89.37%) - LeetSync

---
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    let ans =[[],[]]
+    let longest = Math.max(nums1.length, nums2.length)
+    for (let i = 0; i < longest; i++) {
+        if (i < nums1.length && !nums2.includes(nums1[i]) && !ans[0].includes(nums1[i])) {
+            ans[0].push(nums1[i])
+        }
+        if (i < nums2.length && !nums1.includes(nums2[i]) && !ans[1].includes(nums2[i])) {
+            ans[1].push(nums2[i])
+        }
+    }
+    return ans
+};
