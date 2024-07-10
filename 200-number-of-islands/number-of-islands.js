2805866ef939e3986df670688f2c57be09134cb1
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-07-10 00:06:54 -0400

    Time: 67 ms (76.66%) | Memory: 52.5 MB (61.19%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -1,29 +1,26 @@
 var numIslands = function(grid) {
-    const rows = grid.length;
-    const cols = grid[0].length;
-    let islands = 0;
-    
-    if (!rows || !cols) return islands
-    
-    const dfs = (row, col) => {
-        if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col] !== '1') return
-        grid[row][col] = '0';  
-        
-        dfs(row - 1, col);
-        dfs(row + 1, col);
-        dfs(row, col - 1);
-        dfs(row, col + 1);
-    };
-    
-    for (let row = 0; row < rows; row++) {
-        for (let col = 0; col < cols; col++) {
-            if (grid[row][col] === '1') {
-                dfs(row, col);
-                islands++;
+    let rows = grid.length
+    let cols = grid[0].length
+    let islands = 0
+
+    if (!rows || !cols) return 0
+
+    let dfs = (row,col) => {
+        if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col] !=='1') return
+        grid[row][col] = '0'
+
+        dfs(row, col - 1)
+        dfs(row, col + 1)
+        dfs(row + 1, col)
+        dfs(row - 1, col)
+    }
+    for (let row=0;row<rows;row++){
+        for (let col=0;col<cols;col++){
+            if (grid[row][col] === '1'){
+                dfs(row,col)
+                islands ++
             }
         }
     }
-        console.log("islands", islands)
-        console.log("grid", grid)
-    return islands;
+    return islands
 };
