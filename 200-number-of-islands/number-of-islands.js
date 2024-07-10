ae0b70a253d1e6679550ad27053cce4ff8cc6728
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-07-10 00:13:49 -0400

    Time: 61 ms (91.35%) | Memory: 51.6 MB (90.46%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -2,25 +2,23 @@ var numIslands = function(grid) {
     let rows = grid.length
     let cols = grid[0].length
     let islands = 0
-
-    if (!rows || !cols) return 0
-
+    if(!rows || !cols) return 0
     function dfs(row,col){
-        if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col] !=='1') return
+        if(row<0 || col<0 || col >= cols || row >= rows || grid[row][col] !== '1') return
         grid[row][col] = '0'
 
-        dfs(row, col - 1)
-        dfs(row, col + 1)
-        dfs(row + 1, col)
-        dfs(row - 1, col)
+        dfs(row, col -1)
+        dfs(row, col +1)
+        dfs(row -1, col)
+        dfs(row +1, col)
     }
     for (let row=0;row<rows;row++){
         for (let col=0;col<cols;col++){
-            if (grid[row][col] === '1'){
+            if(grid[row][col] === '1'){
                 dfs(row,col)
                 islands ++
             }
         }
     }
-    return islands
+    return islands 
 };
