253cafde8132cdd4de18d856369c4c8e649d7887
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-09-22 21:27:41 -0700

    Time: 54 ms (41.09%) | Memory: 48.8 MB (50.86%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.js
+++ 1448-maximum-69-number/maximum-69-number.js
@@ -3,13 +3,18 @@
  * @return {number}
  */
 var maximum69Number  = function(num) {
-    let newNum = num.toString().split('');  // Convert number to string, then to array
-    for (let i = 0; i < newNum.length; i++) {
-        if (newNum[i] === '6') {
-            newNum[i] = '9';  // Modify the first '6' to '9'
-            break;  // Stop after changing the first '6'
+  
+    let newNum = num.toString().split("")
+    for (let i=0;i<newNum.length;i++){
+        if(newNum[i] === "6") {
+            newNum[i] = "9"
+            break
         }
+
     }
+
+    return parseInt(newNum.join(''))
+
+
     
-    return parseInt(newNum.join(''));  // Convert array back to string and then to number
 };
