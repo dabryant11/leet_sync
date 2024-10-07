7387299962ac1927db773ed6827e0ac241eac889
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-07 16:56:19 -0700

    Time: 69 ms (47.41%) | Memory: 56.6 MB (32.58%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.js
@@ -1,13 +1,9 @@
 var searchBST = function(root, val) {
-    if (!root) return null;  // Return null if the node doesn't exist
-    if (root.val === val) return root;  // Return the node if the value matches
-    
-    // Since this is a binary search tree, we know:
-    // If val is less than the current node's value, search the left subtree.
-    if (val < root.val) {
-        return searchBST(root.left, val);
-    }
-    
-    // Otherwise, search the right subtree.
-    return searchBST(root.right, val);
+ if ( !root) return null
+ if ( root.val == val) return root
+
+ if ( root.val < val) return searchBST(root.right, val)
+ else return searchBST(root.left,val)
+
+
 };
