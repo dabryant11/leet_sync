85ce45dd1959cd098936b81fef6f928ee2b20b2a
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-08 12:12:42 -0700

    Time: 79 ms (14.00%) | Memory: 52.4 MB (36.20%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.js
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.js
@@ -9,16 +9,14 @@ var frequencySort = function(nums) {
     for (let i=0;i<nums.length;i++){
         if(!map[nums[i]]) map[nums[i]] = 0
         map[nums[i]] ++
-    }
+    }    
 
     let sorted = Object.entries(map).sort((a,b) => a[1] - b[1] || b[0] - a[0])
 
-    sorted.forEach(([num,freq]) => {
-        
-        for (let i=0;i<freq;i++){
-            ans.push(Number(num))
+    sorted.forEach(([key,freq]) => {
+        for(let i=0;i<freq;i++){
+            ans.push(Number(key))
         }
     })
-
     return ans
 };
