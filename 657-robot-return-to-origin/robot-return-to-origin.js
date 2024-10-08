0750a2f765ef378a0c8f96663bc3aa2f03c578fc
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-07 18:30:37 -0700

    Time: 45 ms (99.23%) | Memory: 52.9 MB (12.94%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    let coord = [0,0]
+
+    let moo = moves.split("")
+
+    for (let i=0;i<moves.length;i++){
+        if (moo[i] === "U") coord[0] ++
+        if (moo[i] === "D") coord[0] --
+        if (moo[i] === "R") coord[1] ++
+        if (moo[i] === "L") coord[1] --
+        
+    }
+    if (coord[0] === 0 && coord[1] === 0) return true
+    else return false
+  
+};
