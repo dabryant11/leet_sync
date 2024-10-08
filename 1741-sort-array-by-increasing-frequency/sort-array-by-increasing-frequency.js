dbfcf256d1ed30148930da1b491013b194dc9b9e
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-08 12:08:00 -0700

    Time: 78 ms (15.80%) | Memory: 52.8 MB (19.00%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.js
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.js
@@ -3,24 +3,22 @@
  * @return {number[]}
  */
 var frequencySort = function(nums) {
-    let map = {};
-    let ans = [];
+    let map = {}
+    let ans = []
 
-    // Build frequency map
-    for (let i = 0; i < nums.length; i++) {
-        if (!map[nums[i]]) map[nums[i]] = 0;
-        map[nums[i]]++;
+    for (let i=0;i<nums.length;i++){
+        if(!map[nums[i]]) map[nums[i]] = 0
+        map[nums[i]] ++
     }
 
-    // Convert map to array and sort based on frequency
-    let sorted = Object.entries(map).sort((a, b) => a[1] - b[1] || b[0] - a[0]);
+    let sorted = Object.entries(map).sort((a,b) => a[1] - b[1] || b[0] - a[0])
 
-    // Add the numbers to the result array according to frequency
-    sorted.forEach(([num, freq]) => {
-        for (let i = 0; i < freq; i++) {
-            ans.push(Number(num));
+    sorted.forEach(([num,freq]) => {
+        
+        for (let i=0;i<freq;i++){
+            ans.push(Number(num))
         }
-    });
+    })
 
-    return ans;
+    return ans
 };
