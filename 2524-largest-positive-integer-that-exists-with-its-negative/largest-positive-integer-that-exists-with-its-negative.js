dd2423471b555120e0468129fdce236212dee769
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-09 12:46:55 -0700

    Time: 58 ms (89.02%) | Memory: 50 MB (99.52%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.js
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.js
@@ -3,20 +3,16 @@
  * @return {number}
  */
 var findMaxK = function(nums) {
-    nums.sort((a, b) => a - b); // Sort the array in ascending order
+    nums.sort((a, b) => a - b)
 
-    let left = 0;
-    let right = nums.length - 1;
+    let left = 0
+    let right = nums.length -1
 
-    while (left < right) {
-        if (nums[right] === -nums[left]) {
-            return nums[right]; // Return the positive value when a pair is found
-        } else if (nums[right] > -nums[left]) {
-            right--; // Move right pointer if the positive number is too large
-        } else {
-            left++; // Move left pointer if the negative number is too small
-        }
+    while (left < right){
+        if ( nums[right] === -nums[left]) return nums[right]
+        else if ( nums[right] > - nums[left] ) right --
+        else left ++
     }
 
-    return -1; // Return -1 if no such pair is found
+    return -1
 };
