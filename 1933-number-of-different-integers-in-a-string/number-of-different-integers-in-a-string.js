84a96219c4b13021dfa8c1de52f6fbd727a65a9c
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-08 21:06:38 -0700

    Time: 57 ms (52.94%) | Memory: 48.8 MB (91.18%) - LeetSync

--- 1933-number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.js
+++ 1933-number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.js
@@ -7,7 +7,6 @@ var numDifferentIntegers = function(word) {
     const numbersArray = word.match(/\d+/g);
     if(!numbersArray) return 0
         
-    
     const numbers = numbersArray.map(BigInt);
     const uniqueArray = new Set(numbers);
     return uniqueArray.size
