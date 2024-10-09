f4447037d3c62b7c63b8bafc63687c8a4df26d24
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-08 19:59:13 -0700

    Time: 54 ms (62.35%) | Memory: 49.2 MB (45.88%) - LeetSync

--- 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.js
+++ 1975-minimum-distance-to-the-target-element/minimum-distance-to-the-target-element.js
@@ -5,16 +5,13 @@
  * @return {number}
  */
 var getMinDistance = function(nums, target, start) {
-      let left = start, right = start;
-    
-    // While within bounds of the array
-    while (left >= 0 || right < nums.length) {
-        // Check if the target is found at either left or right position
-        if (left >= 0 && nums[left] === target) return Math.abs(left - start);
-        if (right < nums.length && nums[right] === target) return Math.abs(right - start);
-        
-        // Move one step in both directions
-        left--;
-        right++;
+let left = start
+let right = start
+
+    while (left >= 0 || right < nums.length){
+        if(nums[left] === target) return Math.abs(left - start)
+        if(nums[right] === target) return Math.abs(right - start)
+        left --
+        right ++
     }
 };
