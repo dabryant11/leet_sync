2e0a060ff0deabff19be98588ec383aab4b17447
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-08 20:56:34 -0700

    Time: 43 ms (93.51%) | Memory: 49 MB (49.73%) - LeetSync

--- 2168-check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.js
+++ 2168-check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.js
@@ -3,25 +3,16 @@
  * @return {boolean}
  */
 var areNumbersAscending = function(s) {
-    // Split the string into words
-    const words = s.split(" ");
-    let prevNum = -Infinity; // Initialize prevNum to a very small value
+    let words = s.split(" ")
+    let prevNum = -Infinity 
 
-    // Iterate over each word
-    for (let word of words) {
-        // Check if the word is a number
-        if (!isNaN(word)) {
-            const currentNum = parseInt(word);
-
-            // If current number is not greater than the previous, return false
-            if (currentNum <= prevNum) return false;
-                
-            
-            // Update prevNum to the current number
-            prevNum = currentNum;
+    for ( let word of words){
+        if (!isNaN(word)){
+            let currNum = Number(word)
+            if (prevNum >= currNum) return false
+            prevNum = currNum
         }
-    }
 
-    // If we never returned false, the numbers are ascending
-    return true;
+    }
+    return true
 };
