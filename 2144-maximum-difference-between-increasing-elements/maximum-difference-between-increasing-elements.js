3f9d50db1b57eede0c4a28db4f046d7c58373703
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-26 22:23:12 -0700

    Time: 0 ms (100.00%) | Memory: 49.3 MB (28.97%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.js
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.js
@@ -3,15 +3,19 @@
  * @return {number}
  */
 var maximumDifference = function(nums) {
-    let minNum = nums[0];
-    let maxDiff = -1;
+    //check asc
+    // check biggest 
+    // return biggest
+    let minNum = nums[0]
+    let biggest = -1
 
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[i] > minNum) {
-            maxDiff = Math.max(maxDiff, nums[i] - minNum);
+    for ( let i=1;i<nums.length;i++){
+        if (nums[i] > minNum){
+            biggest = Math.max(biggest, nums[i] - minNum)
         } else {
-            minNum = nums[i];
+            minNum = nums[i]
         }
     }
-    return maxDiff;
+    return biggest 
+    
 };
