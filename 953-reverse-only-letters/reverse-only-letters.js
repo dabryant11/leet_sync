17ca1d9a927fd67f77462aa10db529f18a39e894
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-29 15:30:08 -0700

    Time: 1 ms (57.89%) | Memory: 49.6 MB (33.58%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.js
+++ 953-reverse-only-letters/reverse-only-letters.js
@@ -3,17 +3,17 @@
  * @return {string}
  */
 var reverseOnlyLetters = function(s) {
+    let ans = s.split('')
     let left = 0
     let right = s.length -1
-    let ans = s.split('')
 
     let isLetter = (char) => /^[a-zA-Z]$/.test(char)
 
-    while (left < right){
+    while (left<right){
         if (!isLetter(ans[left])) left ++
         else if (!isLetter(ans[right])) right --
-        else{
-            [ans[left],ans[right]] = [ans[right],ans[left]]
+        else {
+            [ans[left],ans[right]] = [ans[right],ans[left]] 
             left ++
             right --
         }
