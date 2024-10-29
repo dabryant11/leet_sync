b9b3b41737adc54e135e43fb2d5d2e99366f1300
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-29 15:20:06 -0700

    Time: 0 ms (100.00%) | Memory: 49.5 MB (34.50%) - LeetSync

--- 953-reverse-only-letters/reverse-only-letters.js
+++ 953-reverse-only-letters/reverse-only-letters.js
@@ -3,32 +3,21 @@
  * @return {string}
  */
 var reverseOnlyLetters = function(s) {
-    // Convert the string to an array to allow easy manipulation of characters
-    let ans = s.split('');
-    let left = 0;
-    let right = s.length - 1;
+    let ans = s.split("")
+    let left = 0
+    let right = s.length -1
 
-    // Function to check if a character is a letter
-    const isLetter = (char) => /^[a-zA-Z]$/.test(char);
+    let isLetter = (char) => /^[a-zA-Z]$/.test(char)
 
-    // Use two pointers to swap only letters
-    while (left < right) {
-        // Move left pointer until we find a letter
-        if (!isLetter(ans[left])) {
-            left++;
-        }
-        // Move right pointer until we find a letter
-        else if (!isLetter(ans[right])) {
-            right--;
-        }
-        // Both pointers are at letters, so swap them
+    while ( left < right){
+        if (!isLetter(ans[left])) left ++ 
+        else if (!isLetter(ans[right])) right--
         else {
-            [ans[left], ans[right]] = [ans[right], ans[left]];
-            left++;
-            right--;
+            [ans[right], ans[left]] = [ans[left], ans[right]]
+            left ++
+            right --
         }
     }
 
-    // Join the array back into a string
-    return ans.join('');
+    return ans.join('')
 };
