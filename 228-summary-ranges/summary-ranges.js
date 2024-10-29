8c235e89bb56cd0f5545d471b3fa696165318dbf
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-29 12:47:10 -0700

    Time: 0 ms (100.00%) | Memory: 48.9 MB (28.46%) - LeetSync

--- 228-summary-ranges/summary-ranges.js
+++ 228-summary-ranges/summary-ranges.js
@@ -5,15 +5,17 @@
 var summaryRanges = function (nums) {
     let ans = [];
     let start = nums[0];
-    for (let i = 1; i <= nums.length; i++) {
-        if (nums[i] - nums[i - 1] === 1) continue;
-        if (start === nums[i - 1]) {
+    let curr = 1
+    for (let curr =1;curr <= nums.length;curr++) {
+        if (nums[curr] - nums[curr - 1] === 1) continue;
+        if (start === nums[curr - 1]) {
             ans.push(`${start}`);
         }
         else {
-            ans.push(`${start}->${nums[i - 1]}`);
+            ans.push(`${start}->${nums[curr - 1]}`);
         }
-        start = nums[i];
+        start = nums[curr];
+        
     }
     return ans;
 };
