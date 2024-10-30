5b2540c28ab5738e8c5ad4e398fd6bc8eefc5240
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-29 18:36:20 -0700

    Time: 1 ms (76.97%) | Memory: 48.6 MB (92.58%) - LeetSync

--- 392-is-subsequence/is-subsequence.js
+++ 392-is-subsequence/is-subsequence.js
@@ -4,17 +4,13 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    let sPointer = 0;
-    let tPointer = 0;
+    let sPointer = 0
+    let tPointer = 0
 
-    // Loop through t and check if characters of s are in sequence
-    while (sPointer < s.length && tPointer < t.length) {
-        if (s[sPointer] === t[tPointer]) {
-            sPointer++; // Move to the next character in s if we have a match
-        }
-        tPointer++; // Always move to the next character in t
-    }
+    while (sPointer < s.length && tPointer < t.length){
+        if (s[sPointer] === t[tPointer]) sPointer++
+        tPointer ++
+    }   
 
-    // If we've matched all characters in s, return true
-    return sPointer === s.length;
+    return sPointer === s.length
 };
