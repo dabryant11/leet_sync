f114fda64688436c9adcc31f31f15ec4aff75659
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-30 12:18:07 -0700

    Time: 0 ms (100.00%) | Memory: 49 MB (50.98%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.js
+++ 408-valid-word-abbreviation/valid-word-abbreviation.js
@@ -4,22 +4,25 @@
  * @return {boolean}
  */
 var validWordAbbreviation = function(word, abbr) {
-    let ap = 0
-    let wp = 0
-    let num = 0
+   let ap = 0
+   let wp = 0
+   let num = 0
 
-    while  (ap<abbr.length && wp<word.length){
+   while (wp<word.length && ap<abbr.length){
         if(!isNaN(abbr[ap])){
             num = num * 10 + Number(abbr[ap])
             if(num == 0) return false
             ap++
-        } else if(num > 0){
+        } else if (num > 0){
             wp += num
             num = 0
-        } else if(abbr[ap] == word[wp]){
+        } else if (abbr[ap] === word[wp]){
             wp ++
-            ap++
+            ap ++
         } else return false
-    }
-    return wp + num == word.length && ap == abbr.length
+   }
+
+   return wp + num == word.length && ap == abbr.length
+
+
 };
