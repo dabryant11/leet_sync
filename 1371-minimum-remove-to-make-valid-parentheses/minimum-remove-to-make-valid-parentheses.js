d0587bc29ed6c597c65fda34bfd60704d5f49223
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-30 09:45:57 -0700

    Time: 20 ms (64.19%) | Memory: 58.1 MB (39.33%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
@@ -24,27 +24,4 @@ var minRemoveToMakeValid = function(s) {
     }
     return str.join("")
 
-
-    // const stack = [];
-    // const str = s.split('');
-    
-    
-    // for (let i = 0; i < s.length; i++) {
-    //     const ch = s[i]; 
-    
-    //     if (ch == '(')
-    //         stack.push(i);
-    //     else {
-    //         if (ch == ')') {
-    //             if (stack.length) stack.pop();
-    //             else str[i] = '';
-    //         }
-    //     }
-    // }
-    
-    // for (let i of stack) {
-    //     str[i] = '';
-    // }
-    
-    // return str.join('');
 };
