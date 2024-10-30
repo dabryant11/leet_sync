f9c541937f326cdf9ef4de0cb1c0ddc1368d2642
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-30 10:28:21 -0700

    Time: 0 ms (100.00%) | Memory: 48.9 MB (71.95%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.js
+++ 408-valid-word-abbreviation/valid-word-abbreviation.js
@@ -4,36 +4,21 @@
  * @return {boolean}
  */
 var validWordAbbreviation = function(word, abbr) {
-    // setup two pointers and current number
     let i = 0, j = 0, number = 0;
-    // loop while the pointers are within the string bounds
-    while(i < abbr.length && j < word.length) {
-        // if the abbr char is a number
-        if(!isNaN(abbr[i])) {
-            // add the number to the previous number times 10
-			// for example "12" will be 1 first then 10 + 2
+
+    while (i < abbr.length && j < word.length) {
+        if (!isNaN(abbr[i])) {
             number = number * 10 + Number(abbr[i]);
-            // if the new number is zero return false for leading zeros
-            if(number === 0) return false;
-            // increment the abbv pointer
+            if (number === 0) return false;
             i++;
-        }
-        // if the number is greater than zero
-        else if(number > 0) {
-            // move the word pointer by number spaces
-            j+=number;
-            // reset the number
+        } else if (number > 0) {
+            j += number;
             number = 0;
-        } 
-        // if the characters match
-        else if(abbr[i] == word[j]) {
-            // increment both pointers
-            i++; j++;
-        } 
-        // otherwise the characters don't match so return false
-        else return false;
+        } else if (abbr[i] == word[j]) {
+            i++;
+            j++;
+        } else return false;
     }
-    // the abbv is valid if the abbv pointer made it all the way through the string
-    // and the word pointer plus any remaining number made it through the string
+
     return i === abbr.length && j + number === word.length;
 };
