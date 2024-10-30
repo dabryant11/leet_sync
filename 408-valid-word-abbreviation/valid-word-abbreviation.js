a4a04001ce8f18c0e4f15d4c7aff74ff9fbf8f2a
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-30 12:28:40 -0700

    Time: 0 ms (100.00%) | Memory: 48.9 MB (71.95%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.js
+++ 408-valid-word-abbreviation/valid-word-abbreviation.js
@@ -4,25 +4,25 @@
  * @return {boolean}
  */
 var validWordAbbreviation = function(word, abbr) {
-   let ap = 0
-   let wp = 0
-   let num = 0
+    let wp = 0
+    let ap = 0
+    let num = 0
 
-   while (wp<word.length && ap<abbr.length){
+    while ( ap<abbr.length && wp <word.length){
         if(!isNaN(abbr[ap])){
             num = num * 10 + Number(abbr[ap])
-            if(num == 0) return false
-            ap++
+            if (num === 0) return false
+            ap ++
         } else if (num > 0){
             wp += num
             num = 0
-        } else if (abbr[ap] === word[wp]){
+        }
+        else if (abbr[ap] === word[wp]){
             wp ++
             ap ++
         } else return false
-   }
+    }
+    return ap == abbr.length && wp + num === word.length
 
-   return wp + num == word.length && ap == abbr.length
-
-
+    
 };
