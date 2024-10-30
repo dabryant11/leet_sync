0d260a76fb7dd7d9d10fefb7554238a6e51acb44
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-29 17:51:02 -0700

    Time: 53 ms (19.61%) | Memory: 59.1 MB (58.77%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.js
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.js
@@ -3,21 +3,19 @@
  * @return {number}
  */
 var findLHS = function(nums) {
-    let map = {};
-    let maxLen = 0;
+    let map = {}
+    let maxLen = 0
 
-    // Count frequencies of each number in nums
-    for (let num of nums) {
-        map[num] = (map[num] || 0) + 1;
+    for (let i =0;i<nums.length;i++){
+        if(!map[nums[i]])map[nums[i]] =0
+        map[nums[i]] ++
     }
 
-    // Check for harmonious subsequence lengths
-    for (let num in map) {
-        let currNum = parseInt(num);
-        if (map[currNum + 1]) { // Check if there's a number with a difference of 1
-            maxLen = Math.max(maxLen, map[currNum] + map[currNum + 1]);
-        }
+    for (let num in map){
+        let currNum = parseInt(num)
+        if(map[currNum+1]) maxLen = Math.max(maxLen,map[currNum] + map[currNum+1])
     }
+    return maxLen
 
-    return maxLen;
-};
+
+}
