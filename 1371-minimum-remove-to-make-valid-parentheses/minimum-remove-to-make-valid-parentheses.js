26be316fac8ab40a4a635cbc05d5bf575bf8948a
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-30 09:32:08 -0700

    Time: 27 ms (33.62%) | Memory: 55.7 MB (90.53%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
@@ -4,31 +4,23 @@
  */
 
 var minRemoveToMakeValid = function(s) {
-    const stack = []; // Initialize a stack to hold indices of opening parentheses
-    const str = s.split(''); // Convert the string to an array for efficient modification
+    const stack = [];
+    const str = s.split('');
     
-    // Iterate through each character of the string
+    
     for (let i = 0; i < s.length; i++) {
-        const ch = s[i]; // Current character
-        
-        // If an opening parenthesis is encountered, push its index onto the stack
+        const ch = s[i]; 
+    
         if (ch == '(')
             stack.push(i);
         else {
-            // If a closing parenthesis is encountered
             if (ch == ')') {
-                // If there's a matching opening parenthesis in the stack, pop it
-                if (stack.length)
-                    stack.pop();
-                // If there's no matching opening parenthesis, 
-                // remove the current closing parenthesis by setting it to an empty string
-                else
-                    str[i] = '';
+                if (stack.length) stack.pop();
+                else str[i] = '';
             }
         }
     }
     
-    // Remove any remaining unmatched opening parentheses by setting their corresponding characters to an empty string
     for (let i of stack) {
         str[i] = '';
     }
