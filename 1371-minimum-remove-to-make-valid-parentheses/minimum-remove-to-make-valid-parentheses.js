7fde0f28ee2b9a1e4f913e0128e253495db34ffb
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-30 10:18:26 -0700

    Time: 17 ms (77.29%) | Memory: 56.8 MB (67.66%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.js
@@ -6,19 +6,19 @@ var minRemoveToMakeValid = function(s) {
     let stack = []
     let str = s.split("")
 
-    for (let i=0;i<s.length;i++){
+    for ( let i=0;i<s.length;i++){
         let ch = s[i]
-        if(ch == "("){
-            stack.push(i)
-        } else{
-            if(ch == ")"){
+        if (ch == "(") stack.push(i)
+        else {
+            if (ch == ")") {
                 if (stack.length) stack.pop()
                 else str[i] = ""
             }
         }
     }
-    for (let i of stack){
+    for (let i of stack ){
         str[i] = ""
+    
     }
 
     return str.join("")
