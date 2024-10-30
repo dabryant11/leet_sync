3015974cefb73abe7871799e2795f7d09fd30723
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-29 19:49:32 -0700

    Time: 0 ms (100.00%) | Memory: 48.8 MB (52.58%) - LeetSync

--- 290-word-pattern/word-pattern.js
+++ 290-word-pattern/word-pattern.js
@@ -5,23 +5,24 @@
  */
 var wordPattern = function(pattern, s) {
     let words = s.split(" ")
-
     if (words.length !== pattern.length) return false
 
     let charToWord = new Map()
     let wordToChar = new Map()
 
-    for (let i=0;i<words.length;i++){
+    for(let i=0;i<words.length;i++){
         let char = pattern[i]
         let word = words[i]
+
         if(charToWord.has(char)){
             if(charToWord.get(char) !== word) return false
-        } else{
-            charToWord.set(char, word)
+        } else {
+            charToWord.set(char,word)
         }
+
         if(wordToChar.has(word)){
             if(wordToChar.get(word) !== char) return false
-        } else{
+        } else {
             wordToChar.set(word, char)
         }
     }
