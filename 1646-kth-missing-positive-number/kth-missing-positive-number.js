20918402366be3825198b30988486f5754f1d297
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-31 11:15:32 -0700

    Time: 1 ms (54.91%) | Memory: 48.8 MB (81.31%) - LeetSync

---
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+    let missNum = 0; // Counter for missing numbers
+    let current = 1; // Start checking from the first positive integer
+    let i = 0; // Index for traversing arr
+    
+
+    while (missNum < k) {
+        // If current is not in arr, it is a missing number
+        if (i >= arr.length || arr[i] !== current) {
+            missNum++;
+            if (missNum === k) return current;
+        } else {
+            i++; // Move to the next element in arr if current matches
+        }
+        current++; // Move to the next positive integer
+    }
+
+};
