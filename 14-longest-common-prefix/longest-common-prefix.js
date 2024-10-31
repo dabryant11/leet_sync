93ed32c8ac24280fef733633f76ba93524d74bc2
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-31 11:02:52 -0700

    Time: 0 ms (100.00%) | Memory: 49.2 MB (68.31%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -5,14 +5,12 @@
 var longestCommonPrefix = function(strs) {
     strs.sort()
     let firstWord = strs[0]
-    let lastWord = strs[strs.length - 1]
-    let ans = ''
-    for(let i = 0; i < firstWord.length; i++){
-        if(firstWord[i] == lastWord[i]){
-            ans += firstWord[i]
-        }else{
-            break
-        }
+    let lastWord = strs[strs.length -1]
+    let ans = ""
+
+    for (let i=0; i < firstWord.length; i++){
+        if(firstWord[i] === lastWord[i]) ans += firstWord[i]
+        else break
     }
     return ans
 };
