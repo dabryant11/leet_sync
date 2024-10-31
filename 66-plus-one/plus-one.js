bc0718525e495dcc9505af284b4b306727d8361c
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-30 17:19:07 -0700

    Time: 0 ms (100.00%) | Memory: 48.7 MB (71.36%) - LeetSync

--- 66-plus-one/plus-one.js
+++ 66-plus-one/plus-one.js
@@ -6,7 +6,7 @@ var plusOne = function(digits) {
     for (let i = digits.length - 1; i >= 0; i--) {
         // If the current digit is less than 9, just increment it and return the array.
         if (digits[i] < 9) {
-            digits[i] += 1;
+            digits[i] ++;
             return digits;
         }
         // If the digit is 9, set it to 0 and move to the next digit.
