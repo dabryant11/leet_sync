1c0cd609b44affa98c70ed6e417ba9609e4d5ba7
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-30 17:11:45 -0700

    Time: 1 ms (89.14%) | Memory: 50.9 MB (41.85%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -6,9 +6,9 @@
 var twoSum = function(nums, target) {
     let map = new Map()
 
-    for (let i =0;i<nums.length;i++){
+    for (let i=0;i<nums.length;i++){
         let compliment = target - nums[i]
         if (map.has(compliment)) return [map.get(compliment),i]
-        map.set(nums[i],i)
+        map.set(nums[i], i)
     }
 };
