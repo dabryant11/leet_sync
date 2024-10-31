6ead88a17c4650763f3043ad055898ca3bdc8a10
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-10-30 17:08:46 -0700

    Time: 2 ms (75.18%) | Memory: 51.5 MB (26.35%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,20 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = new Map();
+    let map = new Map()
 
-    for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
-
-        // Check if the complement exists in the map
-        if (map.has(complement)) {
-            return [map.get(complement), i];
-        }
-
-        // Store the index of the current number
-        map.set(nums[i], i);
+    for (let i =0;i<nums.length;i++){
+        let compliment = target - nums[i]
+        if (map.has(compliment)) return [map.get(compliment),i]
+        map.set(nums[i],i)
     }
-
-    // Return an empty array if no solution is found (for safety)
-    return [];
 };
