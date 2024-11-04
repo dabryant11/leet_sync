38ad5d8dca200a4ce788655bd32b6b465109be57
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-04 13:46:28 -0800

    Time: 46 ms (69.74%) | Memory: 57.6 MB (73.36%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.js
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.js
@@ -4,35 +4,36 @@
  */
 var shortestPathBinaryMatrix = function(grid) {
     let directions = [
-        [1,0],
-        [-1,0],
+        [0,1],
+        [0,-1],
         [1,1],
         [1,-1],
         [-1,1],
         [-1,-1],
-        [0,-1],
-        [0,1],
+        [1,0],
+        [-1,0],
     ]
 
-    if(grid[0][0] ===1) return -1
+    if(grid[0][0] === 1) return -1
 
-    let queue = [[0,0,1]]
     let N = grid.length
+    let queue = [[0,0,1]]
 
     while (queue.length){
         let [row,col,path] = queue.shift()
-        if(row >= N -1 && col >= N -1) return path
 
-        for (let [dx,dy] of directions){
+        if(row === N -1 && col === N -1) return path
+        for ( let [dx,dy] of directions) {
             let x = row + dx
             let y = col + dy
-            if(x>= N || x< 0) continue
-            if(y>= N || y< 0) continue
-            if(grid[x][y] !== 0) continue
+
+            if (x<0 || x>=N) continue
+            if (y<0 || y>=N) continue
+            if (grid[x][y] !== 0) continue
 
             queue.push([x,y,path + 1])
             grid[x][y] = 1
         }
     }
-    return -1 
+    return -1
 };
