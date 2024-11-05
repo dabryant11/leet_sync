540828e7cc3af033ec61296ef28f3eeac2abef9f
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-05 12:53:05 -0800

    Time: 2552 ms (29.41%) | Memory: 88.1 MB (21.95%) - LeetSync

--- 1142-minimum-knight-moves/minimum-knight-moves.js
+++ 1142-minimum-knight-moves/minimum-knight-moves.js
@@ -3,37 +3,38 @@
  * @param {number} y
  * @return {number}
  */
-var minKnightMoves = function(x, y) {    
+var minKnightMoves = function(x, y) {
     let directions = [
         [2,1],
-        [-2,1],
-        [1,2],
-        [-1,2],
-        [1,-2],
         [2,-1],
         [-2,-1],
+        [-2,1],
+        [1,2],
+        [1,-2],
+        [-1,2],
         [-1,-2],
     ]
+
+    let visited = new Set()
     let moves = 0
     let queue = [[0,0]]
-    let visited = new Set()
-    
+
     while (queue.length !== 0){
         let next = []
         while (queue.length !== 0){
-            let curr = queue.pop()
-            if(curr[0] === x && curr[1] === y) return moves 
-            for ( let d of directions){
+        let curr = queue.pop()
+        if(curr[0] === x && curr[1] === y) return moves
+        
+            for(let d of directions){
                 let nextX = curr[0] + d[0]
                 let nextY = curr[1] + d[1]
                 if(!visited.has(nextX+","+nextY)){
                     next.push([nextX,nextY])
                     visited.add(nextX+","+nextY)
-                } 
+                }
             }
         }
-        moves++
         queue = next
+        moves ++
     }
-    
 };
