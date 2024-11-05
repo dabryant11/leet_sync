f67c7363fe75750d77d4eaf5d4ae4a69ce2b304d
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-04 23:11:32 -0800

    Time: 0 ms (100.00%) | Memory: 49 MB (22.96%) - LeetSync

--- 198-house-robber/house-robber.js
+++ 198-house-robber/house-robber.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var rob = function(nums) {
-   if (nums.length === 0) return 0
-   if (nums.length === 1) return nums[0]
+    if(nums.length === 1) return nums[0]
+    if(nums.length === 0) return 0
 
-   let prev1 = 0
-   let prev2 = 0
+    let prev1 = 0
+    let prev2 = 0
 
-   for (let num of nums){
-    let temp = prev1
-     prev1 = Math.max(prev2 + num,prev1)
-     prev2 = temp
-   }
+    for (let num of nums){
+        let temp = prev1
+        prev1 = Math.max(prev1, prev2 + num)
+        prev2 = temp
+    }
 
-   return prev1
+    return prev1
 };
