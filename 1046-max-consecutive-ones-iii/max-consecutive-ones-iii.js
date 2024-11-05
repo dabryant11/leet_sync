a8e161cbf4064cced5aae17127390e581b0e3526
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-05 15:54:41 -0800

    Time: 3 ms (66.28%) | Memory: 54.4 MB (61.67%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
@@ -4,20 +4,19 @@
  * @return {number}
  */
 var longestOnes = function(nums, k) {
-    let flips = 0
     let left = 0
+    let flips = 0
     let highestStreak = 0
 
     for (let right =0;right<nums.length;right++){
-        if (nums[right] === 0){
-            flips ++
-        }
+        if(nums[right] === 0) flips ++
 
         while (flips>k){
-            if (nums[left] === 0) flips --
+            if(nums[left] === 0) flips --
             left ++
         }
-        highestStreak = Math.max(highestStreak, right - left + 1)
+        highestStreak = Math.max(highestStreak,right-left +1)
     }
+
     return highestStreak
 };
