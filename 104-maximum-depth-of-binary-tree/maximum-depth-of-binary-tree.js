e91a673948f9792a06f0b0ef6848de750fd57e97
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-04 23:16:05 -0800

    Time: 0 ms (100.00%) | Memory: 53 MB (9.02%) - LeetSync

---
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0
+    let leftSide = maxDepth(root.left)
+    let rightSide = maxDepth(root.right)
+    return Math.max(leftSide,rightSide )+ 1
+};
