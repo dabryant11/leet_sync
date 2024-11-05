bc341e425154e605d818b1715e1144af6344401c
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-05 11:28:48 -0800

    Time: 0 ms (100.00%) | Memory: 49.1 MB (32.35%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -3,9 +3,22 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    let highestNum = Math.max(...nums)
+    let left = 0;
+    let right = nums.length - 1;
 
-    for (let i=0;i<nums.length;i++){
-        if (nums[i] === highestNum) return i
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        // Check if the middle element is greater than its next element
+        if (nums[mid] > nums[mid + 1]) {
+            // Peak is in the left half (including mid)
+            right = mid;
+        } else {
+            // Peak is in the right half (excluding mid)
+            left = mid + 1;
+        }
     }
+
+    // `left` and `right` converge to a peak element index
+    return left;
 };
