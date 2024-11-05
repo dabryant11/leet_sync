592122ece89ae49cab4d01c9ff2143a65c7b475b
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-05 11:33:52 -0800

    Time: 2 ms (43.70%) | Memory: 51.7 MB (39.77%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.js
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.js
@@ -11,22 +11,20 @@
  * @return {number[][]}
  */
 var verticalOrder = function(root) {
-     if (!root) return [];
-  // given constraint: the number of nodes in the tree is 100 at maximum
-  let arr = new Array(200);
+if (!root) return [];
   let queue = [];
-  queue.push({ node: root, col: 100 });
+  let m = new Map();
+  queue.push({ node: root, col: 0 });
   while (queue.length > 0) {
     let { node, col } = queue.shift();
-    if (arr[col]) {
-      arr[col].push(node.val);
-    } else {
-      arr[col] = [node.val];
-    }
+    if (m.has(col)) m.get(col).push(node.val);
+    else m.set(col, [node.val]);
     if (node.left) queue.push({ node: node.left, col: col - 1 });
     if (node.right) queue.push({ node: node.right, col: col + 1 });
   }
-  return arr.filter((x) => x !== undefined);
-  // T.C: O(N)
+  m = Array.from(m.entries());
+  m.sort((a, b) => a[0] - b[0]);
+  return m.map((x) => x[1]);
+  // T.C: O(Nlog(N))
   // S.C: O(N)
 };
