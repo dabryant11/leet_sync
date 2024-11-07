3f53baf004c7e897d87326c3846004e961f36f2c
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-07 14:00:05 -0800

    Time: 1 ms (81.25%) | Memory: 51.3 MB (25.32%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.js
+++ 807-custom-sort-string/custom-sort-string.js
@@ -6,29 +6,25 @@
 var customSortString = function(order, s) {
     let charCount = {}
 
-    for(let char of order){
+    for (let char of order){
         charCount[char] = 0
     }
 
-    for(let char of s){
+    for (let char of s){
         if (charCount[char] !== undefined) charCount[char] ++
     }
 
     let sortedS = ""
 
-    for(let char of order){
-        // if (charCount[char]){
+    for (let char of order){
+        if (charCount[char]  !== undefined){
             sortedS += char.repeat(charCount[char])
-        // 
-    }
-
-    for(let char of s){
-        if (charCount[char] === undefined){
-            sortedS += char
         }
     }
 
+    for (let char of s){
+        if (charCount[char] === undefined) sortedS += char
+    }
 
     return sortedS
-
 };
