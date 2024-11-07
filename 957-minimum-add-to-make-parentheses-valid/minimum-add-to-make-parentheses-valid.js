f3125949157c7929bbad145d6d1ee9c491b1534e
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-07 13:36:23 -0800

    Time: 0 ms (100.00%) | Memory: 49 MB (75.58%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.js
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var minAddToMakeValid = function(s) {
-    let ans = 0;
-    let extra = 0;
+    let ans = 0
+    let extra = 0
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === "(") {
-            ans++;
-        } else if (s[i] === ")") {
-            if (ans <= 0) {
-                extra++;
-            } else {
-                ans--;
-            }
-        }
+    for ( let i=0;i<s.length;i++){
+        if(s[i] === "(") ans ++
+        else if(s[i] === ")"){
+            if(ans <=0){
+                extra++
+            } else ans --
+        } 
     }
 
-    return ans + extra;
+    return ans + extra
+
+
 };
