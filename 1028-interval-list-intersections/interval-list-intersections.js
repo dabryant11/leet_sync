5820e0acecfebcc605a9b069403b4618c1248c6d
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-07 21:52:22 -0800

    Time: 3 ms (66.06%) | Memory: 58 MB (63.51%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.js
+++ 1028-interval-list-intersections/interval-list-intersections.js
@@ -5,21 +5,22 @@
  */
 var intervalIntersection = function(firstList, secondList) {
     let ans = []
+
     let i = 0
     let j = 0
 
     while (i<firstList.length && j<secondList.length){
         let firstPair = firstList[i]
-        let secondPair = secondList[j]
+        let secPair = secondList[j]
 
-        let start = Math.max(firstPair[0],secondPair[0])
-        let end = Math.min(firstPair[1],secondPair[1])
+        let start = Math.max(firstPair[0], secPair[0])
+        let end = Math.min(firstPair[1], secPair[1])
 
-       if(start <= end) ans.push([start,end])
-       if(firstPair[1] < secondPair[1]) i++
+        if(start <= end) ans.push([start,end])
+
+        if(firstPair[1] < secPair[1]) i++
         else j++
 
     }
-    
-    return ans 
+    return ans
 };
