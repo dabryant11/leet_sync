9bca894f563b3ab56285c9f59c08dcb7a7dc47d7
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-08 12:53:57 -0800

    Time: 49 ms (98.69%) | Memory: 52.2 MB (11.56%) - LeetSync

--- 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.js
+++ 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.js
@@ -23,9 +23,10 @@ var insert = function(head, insertVal) {
 
     while (next !== head){
         let condition1 = (insertVal >= curr.val && insertVal <= next.val)
-            const condition2 = (insertVal <= curr.val && insertVal <= next.val && next.val < curr.val);
+         // Condition 2 Example: [2, 3, 5, 1]  val = 0
+        let condition2 = (insertVal <= curr.val && insertVal <= next.val && next.val < curr.val);
         // Condition 3 Example: [2, 3, 5, 1]  val = 6
-        const condition3 = (insertVal >= curr.val && next.val < curr.val);
+        let condition3 = (insertVal >= curr.val && next.val < curr.val);
         if(condition1 || condition2 || condition3){
             curr.next = newNode
             newNode.next = next
