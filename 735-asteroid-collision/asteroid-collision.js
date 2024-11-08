3f8a2918dc4d7f2a1ce1be2be338717725ff76fb
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-07 20:06:08 -0800

    Time: 4 ms (72.52%) | Memory: 51.3 MB (57.89%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.js
+++ 735-asteroid-collision/asteroid-collision.js
@@ -3,22 +3,20 @@
  * @return {number[]}
  */
 var asteroidCollision = function(asteroids) {
-   let ans = []
-
-   for ( let i=0;i<asteroids.length;i++){
-        let last = ans[ans.length-1]
+    let ans = []
+    for (let i =0;i<asteroids.length;i++){
         let curr = asteroids[i]
-        
-        if(!ans.length || curr > 0 || last < 0){
-            ans.push(curr)
+        let last = ans[ans.length -1]
 
-        } else if(-curr === last){
+        if (!ans.length || curr > 0 || last < 0){
+            ans.push(curr)
+        } else if (-curr === last){
             ans.pop()
-        } else if (-curr > last){
+        } else if(-curr > last){
             ans.pop()
             i--
         }
-   }
 
-   return ans
+    }
+    return ans
 };
