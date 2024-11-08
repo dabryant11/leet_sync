30122e3e67e665c4a756f819f98e321b3c1a2077
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-08 10:54:22 -0800

    Time: 64 ms (21.08%) | Memory: 50.8 MB (87.74%) - LeetSync

---
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.js
@@ -0,0 +1,39 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right) {
+ *      this.val = val;
+ *      this.left = left;
+ *      this.right = right;
+ *  };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var treeToDoublyList = function(root) {
+      let head = null;
+    let curr = root;
+    const stack = [];
+    let prev = null;
+    while(stack.length> 0 || curr) {
+        while(curr) {
+            stack.push(curr);
+            curr = curr.left;
+        }
+        curr = stack.pop();
+        if(head === null) head = curr;
+        if(prev) {
+            prev.right = curr;
+            curr.left = prev;
+        }
+        prev= curr;
+        curr = curr.right;
+    }
+    if(head){
+        head.left = prev;
+        prev.right = head;
+    }
+
+    return head;
+};
