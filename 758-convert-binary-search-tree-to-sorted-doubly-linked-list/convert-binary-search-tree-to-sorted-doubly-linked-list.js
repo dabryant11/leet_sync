e1c317c5880f96ee40e06cb800a3100c789a41fe
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-08 11:28:20 -0800

    Time: 64 ms (21.08%) | Memory: 51.1 MB (59.13%) - LeetSync

--- 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.js
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.js
@@ -12,28 +12,31 @@
  * @return {_Node}
  */
 var treeToDoublyList = function(root) {
-      let head = null;
-    let curr = root;
-    const stack = [];
-    let prev = null;
-    while(stack.length> 0 || curr) {
-        while(curr) {
-            stack.push(curr);
-            curr = curr.left;
+    let head = null
+    let prev = null
+    let curr = root
+    let stack = []
+
+    while (stack.length > 0 || curr){
+        while (curr){
+            stack.push(curr)
+            curr = curr.left
         }
-        curr = stack.pop();
-        if(head === null) head = curr;
-        if(prev) {
-            prev.right = curr;
-            curr.left = prev;
+
+        curr = stack.pop()
+        if(head === null) head = curr
+        if(prev){
+            prev.right = curr
+            curr.left = prev
         }
-        prev= curr;
-        curr = curr.right;
-    }
-    if(head){
-        head.left = prev;
-        prev.right = head;
+        prev = curr
+        curr = curr.right 
     }
 
-    return head;
+    if(head){
+        head.left = prev
+        prev.right = head 
+    }
+
+    return head
 };
