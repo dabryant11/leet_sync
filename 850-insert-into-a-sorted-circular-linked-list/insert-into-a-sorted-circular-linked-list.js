e7eceae8e85e871b466745b2406bbd8c462f05bc
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-08 12:40:43 -0800

    Time: 75 ms (5.88%) | Memory: 52.1 MB (17.50%) - LeetSync

--- 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.js
+++ 850-insert-into-a-sorted-circular-linked-list/insert-into-a-sorted-circular-linked-list.js
@@ -11,43 +11,32 @@
  * @param {number} insertVal
  * @return {_Node}
  */
-var insert = function(head, insertVal) { 
-    const newNode = new Node(insertVal);
-    if (!head) {
-        newNode.next = newNode;
-        return newNode;
-    } 
-    
-    let curr = head;
-    let next = head.next;
-    
-    while (next != head) {
-        // Condition 1 Example: [2, 3, 5, 1]  val = 4
-        const condition1 = (insertVal >= curr.val && insertVal <= next.val);
-        // Condition 2 Example: [2, 3, 5, 1]  val = 0
-        const condition2 = (insertVal <= curr.val && insertVal <= next.val && next.val < curr.val);
+var insert = function(head, insertVal) {
+    let newNode = new Node(insertVal)
+
+    if(!head){
+        newNode.next = newNode
+        return newNode
+    }
+    let curr = head
+    let next = curr.next
+
+    while (next !== head){
+        let condition1 = (insertVal >= curr.val && insertVal <= next.val)
+            const condition2 = (insertVal <= curr.val && insertVal <= next.val && next.val < curr.val);
         // Condition 3 Example: [2, 3, 5, 1]  val = 6
         const condition3 = (insertVal >= curr.val && next.val < curr.val);
-        
-        if  (condition1 || condition2 || condition3) {
-            curr.next = newNode;
-            newNode.next = next;
-            return head;
+        if(condition1 || condition2 || condition3){
+            curr.next = newNode
+            newNode.next = next
+            return head
         }
-            
-        curr = next;
-        next = next.next;
+        curr = curr.next
+        next = next.next
     }
-    
-    // If we reach this point in the code, have not met any of the three conditions listed above.
-    // By this point, "curr" is pointing to the last node in the cycle, and "next" is back at the head.
-    // The following code will cover conditions like:
-    // [3, 3] val = 0;  -> [3, 3, 0]
-    // [3, 4, 1] val = 2;  -> [3, 4, 1, 2]
-    // [3, 3] val = 5;   -> [3, 3, 5]
-    // [3, 3] val = 3; -> [3, 3, 3]
-    
-    curr.next = newNode;
-    newNode.next = head;
-    return head;
+
+    curr.next = newNode
+    newNode.next = head
+
+    return head
 };
