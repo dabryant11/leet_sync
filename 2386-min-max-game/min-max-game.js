65cee345141cf321225d7e33ec2457c4c72dc22b
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-12 13:15:04 -0800

    Time: 2 ms (57.89%) | Memory: 50.3 MB (71.64%) - LeetSync

--- 2386-min-max-game/min-max-game.js
+++ 2386-min-max-game/min-max-game.js
@@ -3,14 +3,13 @@
  * @return {number}
  */
 var minMaxGame = function(nums) {
-       if (nums.length === 1) return nums[0];
-    if (nums.length === 2) return Math.min(nums[0], nums[1]);
-    
-    const newNums = [];
-    for (let i = 0; i < nums.length; i+=4) {
-        newNums.push(Math.min(nums[i], nums[i+1]));
-        newNums.push(Math.max(nums[i+2], nums[i+3]));
+    if(nums.length === 1) return nums[0]
+    if(nums.length === 2) return Math.min(nums[0],nums[1])
+
+    let newNums = []
+    for(let i =0; i<nums.length; i+=4){
+        newNums.push(Math.min(nums[i],nums[i+1]))
+        newNums.push(Math.max(nums[i+2],nums[i+3]))
     }
-    
-    return minMaxGame(newNums);
+    return minMaxGame(newNums)
 };
