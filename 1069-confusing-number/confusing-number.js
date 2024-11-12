e197a870d15c678995953115491b33e7e2d3d86b
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-11 21:58:44 -0800

    Time: 0 ms (100.00%) | Memory: 49 MB (43.24%) - LeetSync

--- 1069-confusing-number/confusing-number.js
+++ 1069-confusing-number/confusing-number.js
@@ -3,15 +3,20 @@
  * @return {boolean}
  */
 var confusingNumber = function(n) {
-    const rotations = { '0': '0', '1': '1', '6': '9', '8': '8', '9': '6' };
-    const original = n.toString();
-    let rotated = '';
-
-    for (let i = original.length - 1; i >= 0; i--) {
-        const digit = original[i];
-        if (!rotations[digit]) return false; // If digit is 2, 3, 4, 5, or 7
-        rotated += rotations[digit];
+    let rotations = {
+        "0":"0",
+        "1":"1",
+        "6":"9",
+        "8":"8",
+        "9":"6"
     }
+    let numStr = n.toString()
+    let ans = ""
 
-    return rotated !== original;
+    for(let i = numStr.length -1;i>= 0 ;i--){
+        if(!rotations[numStr[i]]) return false
+        ans += rotations[numStr[i]]
+    }
+    return ans !== numStr
+
 };
