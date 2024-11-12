dbd6fd94d3b6dc56bc956e52b0f186d6d014178a
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-11 21:05:13 -0800

    Time: 4 ms (90.00%) | Memory: 52.3 MB (71.05%) - LeetSync

--- 1074-high-five/high-five.js
+++ 1074-high-five/high-five.js
@@ -3,32 +3,27 @@
  * @return {number[][]}
  */
 var highFive = function(items) {
-    let ans = [];
-    let map = {};
+    let ans = []
+    let map = {}
+    for(let i =0;i<items.length;i++){
+        let student = items[i][0]
+        let grade = items[i][1]
+        if(!map[student]) map[student] = []
+        map[student].push(grade)
+    }    
 
-    // Organize grades by student
-    for (let i = 0; i < items.length; i++) {
-        let student = items[i][0];
-        let grade = items[i][1];
-        if (!map[student]) map[student] = [];
-        map[student].push(grade);
+    for(let key of Object.keys(map)){
+        map[key].sort((a,b) => b-a)
+        let sum = 0
+        let top5 = map[key].slice(0,5)
+
+        sum = top5.reduce((acc,grade) => acc + grade,0)
+        let avgGrade = Math.floor(sum/top5.length)
+        ans.push([parseInt(key), avgGrade])
+
     }
+    
 
-    // Calculate the top 5 average for each student
-    for (let key of Object.keys(map)) {
-        // Sort grades in descending order
-        map[key].sort((a, b) => b - a);
-        
-        // Take the top 5 scores only
-        let topFiveGrades = map[key].slice(0, 5);
-        
-        // Calculate the average of the top 5 grades
-        let sum = topFiveGrades.reduce((acc, grade) => acc + grade, 0);
-        let avgGrade = Math.floor(sum / 5);
-        
-        // Push result to answer array
-        ans.push([parseInt(key), avgGrade]);
-    }
-
-    return ans;
+    
+    return ans
 };
