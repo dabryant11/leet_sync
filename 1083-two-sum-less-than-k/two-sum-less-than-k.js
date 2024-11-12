cc819d603655aec2d76029acb4bc0bdfa276e076
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-11 21:32:35 -0800

    Time: 0 ms (100.00%) | Memory: 49.4 MB (49.21%) - LeetSync

--- 1083-two-sum-less-than-k/two-sum-less-than-k.js
+++ 1083-two-sum-less-than-k/two-sum-less-than-k.js
@@ -1,18 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
 var twoSumLessThanK = function(nums, k) {
-    nums.sort((a, b) => a - b);
-    let left = 0;
-    let right = nums.length - 1;
-    let maxSum = -1;
+    nums.sort((a,b)=> a-b)
 
-    while (left < right) {
-        let sum = nums[left] + nums[right];
-        if (sum < k) {
-            maxSum = Math.max(maxSum, sum);
-            left++; // Try a larger sum by moving left pointer right
-        } else {
-            right--; // Try a smaller sum by moving right pointer left
+    let left = 0
+    let right = nums.length -1
+    let maxSum = -1
+
+//  [34,23,1,24,75,33,54,8]
+//  L                R
+//  [1,8,23,24,33,34,54,75]
+
+    while (left<right){
+        let sum = nums[left] + nums[right]
+        if( sum >= k) right --
+        else {
+            maxSum = Math.max(maxSum,sum)
+            left++
         }
     }
-
-    return maxSum;
+    return maxSum 
 };
