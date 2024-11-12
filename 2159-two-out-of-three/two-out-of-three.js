9516fb3df739fbadb90ff40cb8287184ec361df9
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-12 13:00:11 -0800

    Time: 0 ms (100.00%) | Memory: 53.6 MB (31.31%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.js
+++ 2159-two-out-of-three/two-out-of-three.js
@@ -5,18 +5,21 @@
  * @return {number[]}
  */
 var twoOutOfThree = function(nums1, nums2, nums3) {
-    let obj={}
-    let res=[]
-    for(let num of nums1){
-        obj[num]=1
+    let ans = []
+    let map = {}
+
+    for (let num of nums1){
+        map[num] = 1
     }
-    for(let num of nums2){
-        if(!obj[num]||obj[num]==2) obj[num]=2
-        else if(!res.includes(num)) res.push(num)
+
+    for (let num of nums2){
+        if (!map[num] || map[num] == 2) map[num] = 2
+        else if (!ans.includes(num)) ans.push(num)
     }
-    for(let num of nums3){
-        if(!obj[num]||obj[num]==3) obj[num]=3
-        else if(!res.includes(num)) res.push(num)
+
+    for (let num of nums3){
+        if (!map[num] || map[num] == 3) map[num] = 3
+        else if (!ans.includes(num)) ans.push(num)
     }
-    return res
+    return ans
 };
