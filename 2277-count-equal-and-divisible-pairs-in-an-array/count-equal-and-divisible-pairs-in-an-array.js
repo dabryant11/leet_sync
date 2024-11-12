a0748dd539542ff73bfd980ef4ca191ee2017eb3
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-12 14:26:49 -0800

    Time: 5 ms (20.00%) | Memory: 51.4 MB (5.35%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.js
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.js
@@ -4,13 +4,27 @@
  * @return {number}
  */
 var countPairs = function(nums, k) {
-    var count=0;
-    for(let i=0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]==nums[j]&&(i*j%k==0)){
-                count++;
+    let count = 0;
+    let map = {}; // Map to store indices of each number
+    
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        
+        // Check if we have previous indices for this number
+        if (map[num]) {
+            for (let j of map[num]) {
+                if (i * j % k === 0) {
+                    count++;
+                }
             }
         }
+        
+        // Add the current index to the map for the current number
+        if (!map[num]) {
+            map[num] = [];
+        }
+        map[num].push(i);
     }
+    
     return count;
 };
