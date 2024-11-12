b9304677de9925fd5070965a3b90876ded079f2f
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-12 14:04:27 -0800

    Time: 0 ms (100.00%) | Memory: 49.3 MB (15.09%) - LeetSync

--- 2433-best-poker-hand/best-poker-hand.js
+++ 2433-best-poker-hand/best-poker-hand.js
@@ -7,20 +7,19 @@ var bestHand = function(ranks, suits) {
     let rankMap = new Map()
     let suitMap = new Map()
 
-    for(let rank of ranks){
+    for ( let rank of ranks){
         rankMap.set(rank,(rankMap.get(rank) || 0) +1)
-    }  
+    }
 
-    for(let suit of suits){
+    for ( let suit of suits){
         suitMap.set(suit,(suitMap.get(suit) || 0) +1)
-    }  
-
+    }
     if(suitMap.size === 1) return "Flush"
 
-    let maxRank = Math.max(...rankMap.values())
+    let topRank = Math.max(...rankMap.values())
+    if(topRank >= 3) return "Three of a Kind"
+    else if(topRank === 2) return "Pair"
+    return "High Card"
 
-    if(maxRank>= 3) return "Three of a Kind"
-    if(maxRank === 2) return "Pair"
-    else return "High Card"
 
 };
