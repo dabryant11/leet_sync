3d57070ce4b3a92af9641bdf10c91d5af5623f5e
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-11 21:16:15 -0800

    Time: 3 ms (90.00%) | Memory: 52.1 MB (73.68%) - LeetSync

--- 1074-high-five/high-five.js
+++ 1074-high-five/high-five.js
@@ -5,25 +5,22 @@
 var highFive = function(items) {
     let ans = []
     let map = {}
-    for(let i =0;i<items.length;i++){
+
+    for(let i=0;i<items.length;i++){
         let student = items[i][0]
         let grade = items[i][1]
         if(!map[student]) map[student] = []
         map[student].push(grade)
-    }    
+    }
 
     for(let key of Object.keys(map)){
         map[key].sort((a,b) => b-a)
-        let sum = 0
         let top5 = map[key].slice(0,5)
-
-        sum = top5.reduce((acc,grade) => acc + grade,0)
+        let sum = top5.reduce((acc,grade) => acc+ grade,0)
         let avgGrade = Math.floor(sum/top5.length)
-        ans.push([parseInt(key), avgGrade])
-
+        ans.push([parseInt(key),avgGrade])
     }
     
-
-    
     return ans
+
 };
