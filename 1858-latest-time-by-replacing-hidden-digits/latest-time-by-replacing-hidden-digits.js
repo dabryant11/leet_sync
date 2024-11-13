d594f57d20f2cb6239df21ae0a3da4a6cd19764b
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-13 11:34:55 -0800

    Time: 0 ms (100.00%) | Memory: 48.4 MB (81.82%) - LeetSync

--- 1858-latest-time-by-replacing-hidden-digits/latest-time-by-replacing-hidden-digits.js
+++ 1858-latest-time-by-replacing-hidden-digits/latest-time-by-replacing-hidden-digits.js
@@ -3,23 +3,20 @@
  * @return {string}
  */
 var maximumTime = function(time) {
-    // Convert time string to an array to allow modification
-    let timeArr = time.split("");
+    let timeArr = time.split("")
 
-    for (let i = 0; i < timeArr.length; i++) {
-        if (timeArr[i] === "?") {
-            if (i === 0) {
-                timeArr[i] = (timeArr[1] === "?" || timeArr[1] <= "3") ? "2" : "1";
-            } else if (i === 1) {
-                timeArr[i] = (timeArr[0] === "2") ? "3" : "9";
-            } else if (i === 3) {
-                timeArr[i] = "5";
-            } else if (i === 4) {
-                timeArr[i] = "9";
+    for(let i=0;i<timeArr.length;i++){
+        if(timeArr[i] === "?"){
+            if(i === 0){
+                timeArr[i] = (timeArr[1] === "?" || timeArr[1] <= "3") ? "2":  "1"
             }
+            if(i === 1){
+                timeArr[i] = (timeArr[0] === "2" ) ? "3":  "9"
+            }
+            if(i === 3)timeArr[3] = "5"
+            if(i === 4)timeArr[4] = "9"
         }
     }
 
-    // Join array back to string
-    return timeArr.join("");
+    return timeArr.join("")
 };
