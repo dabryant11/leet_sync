3a028933b867f9a892dec155004e507e78280121
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-13 20:40:31 -0800

    Time: 1 ms (56.98%) | Memory: 52.8 MB (7.34%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.js
+++ 861-flipping-an-image/flipping-an-image.js
@@ -3,19 +3,23 @@
  * @return {number[][]}
  */
 var flipAndInvertImage = function(image) {
-    for (let i =0;i<image.length;i++){
-        let row = image[i]
-        let left = 0
-        let right = row.length -1
-        while (left < right){
-            [row[left],row[right]] = [row[right],row[left]] 
-            right --
-            left ++
-        }
-        for(let j=0;j<row.length;j++){
-            if(row[j] === 1) row[j] = 0
-            else row[j] = 1
+    for (let i = 0; i < image.length; i++) {
+        let row = image[i];
+        let left = 0;
+        let right = row.length - 1;
+
+        // Flip and invert simultaneously
+        while (left <= right) {
+            // Swap and invert both elements if they are not in the middle
+            if (left < right) {
+                [row[left], row[right]] = [1 - row[right], 1 - row[left]];
+            } else {
+                // If left == right, just invert the middle element
+                row[left] = 1 - row[left];
+            }
+            left++;
+            right--;
         }
     }
-    return image
+    return image;
 };
