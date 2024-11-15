ce638ac5f6798a647ab07ed03bb6fe39b06ee27a
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-14 18:27:30 -0800

    Time: 66 ms (91.95%) | Memory: 53.4 MB (82.17%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.js
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.js
@@ -11,21 +11,16 @@
  * @return {number[]}
  */
 var postorder = function(root) {
-     if (!root) return [];
-    
-    let stack = [root];
-    let result = [];
-    
-    while (stack.length > 0) {
-        let node = stack.pop();
-        result.push(node.val);
-        
-        // Push the children to the stack in the order they appear
-        for (let child of node.children) {
-            stack.push(child);
+    if(!root) return []
+    let ans = []
+    let stack = [root]
+    while(stack.length>0){
+        let node = stack.pop()
+        ans.push(node.val)
+        for(let child of node.children){
+            stack.push(child)
         }
     }
-    
-    // Reverse the result to get the correct postorder
-    return result.reverse();
+
+    return ans.reverse()
 };
