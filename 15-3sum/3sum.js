aa8d290e5e940273dbcbae5fe060aff1b9a14541
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-16 14:13:54 -0800

    Time: 33 ms (83.53%) | Memory: 65.1 MB (84.15%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -17,7 +17,7 @@ var threeSum = function(nums) {
             if (sum == 0){
                 ans.push([nums[i],nums[left],nums[right]])
                 while (left <right && nums[left] === nums[left+1]) left ++
-                while (left <right && nums[right] === nums[right+1]) right --
+                while (left <right && nums[right] === nums[right-1]) right --
                 left ++
                 right --
             } else if ( sum > 0) right --
