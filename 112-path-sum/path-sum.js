7b4dd34cab3cd4c9382a3f34beac26de8653032b
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-16 12:44:37 -0800

    Time: 0 ms (100.00%) | Memory: 52.8 MB (31.24%) - LeetSync

---
+++ 112-path-sum/path-sum.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    
+    if(!root) return false
+
+    function dfs(root,currPathSum){
+        if(!root) return false
+        currPathSum += root.val
+        if(!root.left && !root.right) return currPathSum === targetSum
+        return  dfs(root.left,currPathSum) || dfs(root.right,currPathSum)
+    }
+    
+    return dfs(root,0)
+};
