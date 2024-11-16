a70068aa6c7ec8570cb3324686c7614625167101
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-16 13:50:48 -0800

    Time: 0 ms (100.00%) | Memory: 51.8 MB (14.13%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.js
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.js
@@ -11,35 +11,27 @@
  * @return {number[][]}
  */
 var zigzagLevelOrder = function(root) {
-    if (!root) return [];
-    
-    let ans = [];
-    let queue = [root];  // Initialize a queue for BFS
-    let leftToRight = true;  // This flag determines the direction of traversal for each level
+    if(!root) return []
 
-    while (queue.length > 0) {
-        let levelSize = queue.length;
-        let currLevel = [];
+    let queue = [root]
+    let ans = []
+    let leftToRight = true
 
-        // Traverse the current level
-        for (let i = 0; i < levelSize; i++) {
-            let node = queue.shift(); // Dequeue a node
-            
-            currLevel.push(node.val); // Add node value to the current level
-            
-            // Enqueue left and right children
-            if (node.left) queue.push(node.left);
-            if (node.right) queue.push(node.right);
+    while (queue.length > 0){
+        let levelSize = queue.length
+        let currLevel = []
+
+        for(let i =0;i<levelSize;i++){
+            let node = queue.shift()
+            currLevel.push(node.val)
+            if(node.left)queue.push(node.left)
+            if(node.right)queue.push(node.right)
         }
 
-        // If we are on an odd level (i.e., leftToRight is false), reverse the current level
-        if (!leftToRight) {
-            currLevel.reverse();
-        }
-
-        ans.push(currLevel);  // Add the current level to the result
-        leftToRight = !leftToRight;  // Toggle the direction for the next level
+        if (!leftToRight) currLevel.reverse()
+        ans.push(currLevel)
+        leftToRight = !leftToRight
     }
-    
-    return ans;
+
+    return ans
 };
