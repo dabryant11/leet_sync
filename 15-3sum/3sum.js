cf43feeb189e85d36096fdc7542bfc332faffa5e
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-16 14:13:17 -0800

    Time: 34 ms (77.63%) | Memory: 65.3 MB (78.00%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,36 +3,27 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    nums.sort((a, b) => a - b); // Sort the array to enable two-pointer approach
-    let ans = [];
+    let ans = []
+    nums.sort((a,b) => a-b)
 
-    for (let i = 0; i < nums.length - 2; i++) {
-        // Skip duplicates for the 'i' pointer
-        if (i > 0 && nums[i] === nums[i - 1]) continue;
+    for (let i =0;i<nums.length - 2;i++){
+        if (i> 0 &&nums[i] === nums[i-1]) continue
+        let left = i + 1
+        let right = nums.length -1
 
-        let left = i + 1;
-        let right = nums.length - 1;
+        while (left < right){
+            let sum = nums[i] + nums[left] + nums[right]
 
-        while (left < right) {
-            let sum = nums[i] + nums[left] + nums[right];
-
-            if (sum === 0) {
-                ans.push([nums[i], nums[left], nums[right]]);
-                // Skip duplicates for the 'left' pointer
-                while (left < right && nums[left] === nums[left + 1]) left++;
-                // Skip duplicates for the 'right' pointer
-                while (left < right && nums[right] === nums[right - 1]) right--;
-
-                // Move both pointers after finding a valid triplet
-                left++;
-                right--;
-            } else if (sum < 0) {
-                left++;  // We need a larger sum, so move 'left' pointer right
-            } else {
-                right--;  // We need a smaller sum, so move 'right' pointer left
-            }
+            if (sum == 0){
+                ans.push([nums[i],nums[left],nums[right]])
+                while (left <right && nums[left] === nums[left+1]) left ++
+                while (left <right && nums[right] === nums[right+1]) right --
+                left ++
+                right --
+            } else if ( sum > 0) right --
+            else left ++
         }
     }
 
-    return ans;
+    return ans
 };
