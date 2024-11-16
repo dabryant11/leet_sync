3bb80adeddacf7d1cc2d3d66cfd7e0a9858aa177
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-16 11:45:36 -0800

    Time: 2 ms (58.92%) | Memory: 54.6 MB (41.89%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -5,17 +5,19 @@
 var moveZeroes = function(nums) {
     let left = 0
     let right = left + 1
-    while (right <= nums.length -1){
-        if (nums[left] !== 0){
+
+    while (right <nums.length){
+        if(nums[left] !== 0){
             left ++
             right ++
         } else {
             if (nums[right] !== 0){
-                [nums[left],nums[right]] = [nums[right],nums[left]] 
+                [nums[left], nums[right]] = [nums[right], nums[left]]
                 left ++
             }
             right ++
         }
     }
+
     return nums
 };
