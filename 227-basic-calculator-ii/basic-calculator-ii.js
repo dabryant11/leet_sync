eae591d76dcb4ef0fe400f8a3c8a198b69366d36
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-16 22:36:38 -0800

    Time: 14 ms (97.09%) | Memory: 58.1 MB (68.56%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.js
+++ 227-basic-calculator-ii/basic-calculator-ii.js
@@ -3,28 +3,27 @@
  * @return {number}
  */
 var calculate = function (s) {
+    let num = ""
+    let stack = []
+    let prevOp = "+"
+
+    for ( let i =0;i<s.length;i++){
+        let ch = s[i]
+
+        if(ch >= "0" && ch <= "9") num += ch
+
+        
     
-    let num = '';
-    let prevOperator = '+';
-    const stack = [];
+        if ((ch< "0" ||ch > "9") && ch !== " " || i === s.length - 1 ) {
+            if(prevOp === "+")stack.push(parseInt(num))
+            if(prevOp === "-")stack.push(-parseInt(num))
+            if(prevOp === "*")stack.push(stack.pop() * parseInt(num))
+            if(prevOp === "/")stack.push( Math.trunc(stack.pop() /parseInt(num)))
 
-    for (let i = 0; i <= s.length; i++) {
-        const ch = s[i];
-
-        if (ch >= '0' && ch <= '9') {
-            num += ch;
-        }
-
-        if ((ch < '0' || ch > '9') && ch !== ' ' || i === s.length) {
-            if (prevOperator === '+') stack.push(Number(num));
-            if (prevOperator === '-') stack.push(-Number(num));
-            if (prevOperator === '*') stack.push(stack.pop() * Number(num));
-            if (prevOperator === '/') stack.push(Math.trunc(stack.pop() / Number(num)));
-
-            prevOperator = ch;
-            num = '';
+            prevOp = ch
+            num = ""
         }
     }
 
-    return stack.reduce((total, cur) => total + cur, 0);
+    return stack.reduce((acc,curr) => acc+curr,0)
 };
