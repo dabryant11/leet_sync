de24852e03cc738d9b57ce39cbfb6a5afedb60d2
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-17 23:10:14 -0800

    Time: 0 ms (100.00%) | Memory: 48.9 MB (77.37%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -8,10 +8,10 @@
 var merge = function(nums1, m, nums2, n) {
     let p1 = m -1
     let p2 = n -1
-    let p = m + n -1
+    let p = n + m -1
 
-    while (p2 >= 0){
-        if(p1>= 0 && nums1[p1] > nums2[p2]){
+    while (p2>= 0){
+        if (p1>= 0 && nums1[p1] > nums2[p2]){
             nums1[p] = nums1[p1]
             p1--
         } else {
@@ -20,5 +20,4 @@ var merge = function(nums1, m, nums2, n) {
         }
         p--
     }
-
 };
