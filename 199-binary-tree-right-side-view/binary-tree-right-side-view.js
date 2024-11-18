97fb6d600da5c954be26dc8eb73bf955ef7c6bad
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-17 23:25:31 -0800

    Time: 0 ms (100.00%) | Memory: 50.9 MB (72.60%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.js
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.js
@@ -11,26 +11,26 @@
  * @return {number[]}
  */
 var rightSideView = function(root) {
-   if (!root) return [];
+    // bfs
+    /// at end of each level ppush val to arr
+    // return arr
+    // TC oN
+    // SC oW
+    if (!root) return []
 
-    let ans = [];
-    let queue = [root];
+    let ans = []
+    let queue = [root]
 
-    while (queue.length > 0) {
-        let levelSize = queue.length;
 
-        for (let i = 0; i < levelSize; i++) {
-            let node = queue.shift();
-
-            // Capture the rightmost node at each level
-            if (i === levelSize - 1) {
-                ans.push(node.val);
-            }
-
-            if (node.left) queue.push(node.left);
-            if (node.right) queue.push(node.right);
+    while(queue.length > 0){
+        let levelSize = queue.length
+        for(let i =0; i< levelSize;i++ ){
+            let node = queue.shift()
+            if ( i === levelSize - 1) ans.push(node.val)
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
         }
     }
 
-    return ans;
+    return ans
 };
