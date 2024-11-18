4c8dfcf14b1082191f1fc438ca29d0526fbef0ce
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-17 21:29:13 -0800

    Time: 0 ms (100.00%) | Memory: 49.6 MB (19.72%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.js
+++ 408-valid-word-abbreviation/valid-word-abbreviation.js
@@ -4,26 +4,23 @@
  * @return {boolean}
  */
 var validWordAbbreviation = function(word, abbr) {
-    let i = 0; // Pointer for word
-    let j = 0; // Pointer for abbr
-    
-    while (i < word.length && j < abbr.length) {
-        if (!isNaN(abbr[j]) && abbr[j] !== '0') {
-            // Extract the number from abbr
-            let num = "";
-            while (j < abbr.length && !isNaN(abbr[j])) {
-                num += abbr[j];
-                j++;
+    let i = 0
+    let j = 0
+
+    while ( i<word.length && j <abbr.length){
+        if (!isNaN(abbr[j])){
+            if( abbr[j] === "0") return false
+            let num = ""
+            while (j<abbr.length && !isNaN(abbr[j])){
+                num += abbr[j]
+                j ++
             }
-            i += parseInt(num); // Skip characters in word
-        } else if (word[i] === abbr[j]) {
-            i++;
-            j++;
-        } else {
-            return false;
-        }
+            i += parseInt(num)
+        } else if (word[i] === abbr[j]){
+            i++
+            j++
+        } else return false
     }
-    
-    // Ensure both pointers reach the end
-    return i === word.length && j === abbr.length;
+
+    return i === word.length && j === abbr.length
 };
