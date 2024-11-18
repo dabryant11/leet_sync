ccdd60129558e6e7ebc28a30c0fb3bfa1b44457c
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-17 22:57:05 -0800

    Time: 1 ms (92.00%) | Memory: 51.6 MB (26.30%) - LeetSync

--- 71-simplify-path/simplify-path.js
+++ 71-simplify-path/simplify-path.js
@@ -3,15 +3,15 @@
  * @return {string}
  */
 var simplifyPath = function(path) {
-  let ans = []
-  let journey = path.split("/")
+    let ans = []
+    let journey = path.split("/")
 
-  for(let level of journey){
-    if(!level || level === ".") continue
-    else if(level === ".."){
-        if(ans.length > 0) ans.pop()
-    } else ans.push(level)
-  }
+    for (let level of journey){
+        if (!level || level === ".") continue
+        else if (level === "..") {
+            if(ans.length > 0) ans.pop()
+        } else ans.push(level)
+    }
+
     return "/" + ans.join("/")
 };
-
