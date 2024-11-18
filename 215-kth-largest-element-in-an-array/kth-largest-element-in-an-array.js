9568ece1146121df0a92d2c0ebac9d5d8d924e01
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-17 21:35:57 -0800

    Time: 170 ms (13.08%) | Memory: 74.6 MB (7.72%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -5,9 +5,10 @@
  */
 var findKthLargest = function(nums, k) {
     let minHeap = new MinPriorityQueue()
-    for (let num of nums){
+
+    for(let num of nums){
         minHeap.enqueue(num)
-        if(minHeap.size() >k) minHeap.dequeue()
+        while (minHeap.size() > k) minHeap.dequeue()
     }
     return minHeap.front().element
 };
