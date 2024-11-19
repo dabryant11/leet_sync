3b5fb81271c3abfd787b724b54d41b84b9da7ef0
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-18 22:47:34 -0800

    Time: 5 ms (53.05%) | Memory: 53 MB (33.07%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -3,14 +3,12 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
+    let majority = Math.floor(nums.length /2)
     let map = {}
 
-    for(let num of nums){
+    for ( let num of nums){
         if(!map[num]) map[num] = 0
         map[num] ++
-    }
-
-    for(let key in map){
-        if(map[key] >= nums.length /2) return parseInt(key)
+        if (map[num] > majority) return parseInt(num)
     }
 };
