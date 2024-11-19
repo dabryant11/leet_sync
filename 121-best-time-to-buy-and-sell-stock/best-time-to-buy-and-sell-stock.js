66cf304d174fe551289687e661929f570d64a70d
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-18 22:44:08 -0800

    Time: 1 ms (97.38%) | Memory: 59.3 MB (34.42%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,24 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let minPrice = Infinity;  // Track the minimum price
-    let profit = 0;  // Track the maximum profit
-    
-    for (let i = 0; i < prices.length; i++) {
-        // Update the minimum price if a lower price is found
-        if (prices[i] < minPrice) {
-            minPrice = prices[i];
-        }
-        
-        // Calculate the profit by comparing current price with minPrice
-        let currentProfit = prices[i] - minPrice;
-        
-        // Update profit if the current profit is greater than the previous maximum profit
-        // if (currentProfit > profit) {
-            profit = Math.max(currentProfit, profit)
-        // }
-    }
-    
-    return profit;
-};
+    let profit = 0
+    let minPrice = Infinity
 
+    for(let i =0;i<prices.length;i++){
+        if(minPrice > prices[i]){
+            minPrice = prices[i]
+        } else {
+            profit = Math.max(profit, prices[i] - minPrice)
+        }
+    }
+
+    return profit
+};
