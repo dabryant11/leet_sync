f5840f70a88e90031273a182fdb67cc54248f04d
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-19 19:47:30 -0800

    Time: 10 ms (89.66%) | Memory: 54.6 MB (67.90%) - LeetSync

--- 829-subdomain-visit-count/subdomain-visit-count.js
+++ 829-subdomain-visit-count/subdomain-visit-count.js
@@ -7,14 +7,14 @@ var subdomainVisits = function(cpdomains) {
 
     for(let domain of cpdomains){
         let splitCurr = domain.split(" ")
-        let visits = splitCurr[0]
+        let visits = new Number(splitCurr[0])
         let subDomain = splitCurr[1].split(".")
 
         while(subDomain.length > 0){
             let domain = subDomain.join(".")
             if(map.has(domain)){
-                map.set(domain,Number(visits) + map.get(domain))
-            } else map.set(domain,Number(visits))
+                map.set(domain,visits + map.get(domain))
+            } else map.set(domain,visits)
 
             subDomain.shift()
         }
