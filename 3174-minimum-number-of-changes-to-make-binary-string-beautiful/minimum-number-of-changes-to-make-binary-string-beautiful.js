7a83da9fcef1642f72788968653ca00928a07f9a
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-20 10:47:47 -0800

    Time: 3 ms (67.46%) | Memory: 51.5 MB (55.36%) - LeetSync

---
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minChanges = function(s) {
+    let cnt = 0;
+        for (let i = 1; i < s.length; i += 2) {
+            if (s[i] !== s[i - 1]) cnt++;
+        }
+        return cnt;
+};
