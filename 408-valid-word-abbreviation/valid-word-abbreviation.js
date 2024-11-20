c188227b1c9a09cac60b42037a6f1b9355aac97e
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-20 08:43:43 -0800

    Time: 0 ms (100.00%) | Memory: 49.3 MB (28.22%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.js
+++ 408-valid-word-abbreviation/valid-word-abbreviation.js
@@ -4,23 +4,28 @@
  * @return {boolean}
  */
 var validWordAbbreviation = function(word, abbr) {
-    let i = 0
-    let j = 0
+    // 2pointers
+    // same check if abbr num  incre word pointer
+    // 2 pointer end check
 
-    while ( i<word.length && j <abbr.length){
-        if (!isNaN(abbr[j])){
-            if( abbr[j] === "0") return false
-            let num = ""
-            while (j<abbr.length && !isNaN(abbr[j])){
-                num += abbr[j]
-                j ++
+    let wp = 0
+    let ap = 0
+
+    while (ap < abbr.length && wp<word.length){
+        if(!isNaN(abbr[ap])){
+            if( abbr[ap] === "0") return false
+            let num = ''
+            while (wp<word.length && !isNaN(abbr[ap])){
+                num+=abbr[ap]
+                ap++
             }
-            i += parseInt(num)
-        } else if (word[i] === abbr[j]){
-            i++
-            j++
-        } else return false
+            wp += parseInt(num)
+        }
+        else if( word[wp] === abbr[ap]){
+            ap ++
+            wp ++
+        }
+        else return false
     }
-
-    return i === word.length && j === abbr.length
+    return wp === word.length  && ap === abbr.length 
 };
