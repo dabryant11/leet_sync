6af915a039a7ab9d0fdcfb7ebb0a82a48cf6b9fa
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-19 19:47:04 -0800

    Time: 11 ms (87.36%) | Memory: 53.8 MB (85.80%) - LeetSync

--- 829-subdomain-visit-count/subdomain-visit-count.js
+++ 829-subdomain-visit-count/subdomain-visit-count.js
@@ -5,17 +5,19 @@
 var subdomainVisits = function(cpdomains) {
     let map = new Map()
 
-    for ( let domain of cpdomains){
-        let splitD = domain.split(" ")
-        let visits = splitD[0]
-        let subD = splitD[1].split(".")
-        while (subD.length > 0){
-            let currD = subD.join(".")
-            if(map.has(currD)){
-                map.set(currD, Number(visits) + map.get(currD))
-            } else map.set(currD, Number(visits))
-            subD.shift()
+    for(let domain of cpdomains){
+        let splitCurr = domain.split(" ")
+        let visits = splitCurr[0]
+        let subDomain = splitCurr[1].split(".")
+
+        while(subDomain.length > 0){
+            let domain = subDomain.join(".")
+            if(map.has(domain)){
+                map.set(domain,Number(visits) + map.get(domain))
+            } else map.set(domain,Number(visits))
+
+            subDomain.shift()
         }
     }
-    return Array.from(map,([key,value]) => `${value} ${key}`)
+    return Array.from(map,([key,value]) =>`${value} ${key}` )
 };
