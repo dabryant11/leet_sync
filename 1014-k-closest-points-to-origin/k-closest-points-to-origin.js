b0eab8e0b252367dd3eed27fb9aa09cb25cc7fb5
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-20 11:13:32 -0800

    Time: 53 ms (57.89%) | Memory: 63.6 MB (72.04%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kClosest = function(points, k) {
+    var heap = new MaxPriorityQueue({priority: (val) => val[0]});
+
+    for (let i of points) {
+        var distance = i[0] * i[0] + i[1] * i[1];
+        heap.enqueue([distance, i]);
+        if (heap.size() > k) heap.dequeue();
+    }
+
+    var result = [];
+    while(heap.size() > 0) {
+        result.push(heap.dequeue().element[1]);
+    }
+    return result;
+};
