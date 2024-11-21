61765469f73e38ed23635689eae69a4fa00877e3
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-20 23:08:34 -0800

    Time: 25 ms (29.37%) | Memory: 57.1 MB (51.29%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.js
+++ 498-diagonal-traverse/diagonal-traverse.js
@@ -3,15 +3,17 @@
  * @return {number[]}
  */
 var findDiagonalOrder = function(mat) {
-    let rows = mat.length;
-    let cols = mat[0].length;
-    let result = new Array(rows + cols - 1).fill(null).map(() => []);
+    let rows = mat.length
+    let cols = mat[0].length
+    let arr = new Array(rows+cols -1).fill(null).map(()=>[])
 
-    for(let row = 0; row < rows; row++) {
-        for(let col = 0; col < cols; col++) {
-            if((row + col) % 2 === 0) result[row + col].unshift(mat[row][col]);
-            else result[row + col].push(mat[row][col]);   
-        }
+    for(let row=0; row<rows; row++){
+        for(let col=0; col<cols; col++){
+            if((row + col) % 2 === 0) arr[row + col].unshift(mat[row][col])
+            else arr[row + col].push(mat[row][col])
+        }   
     }
-    return result.flat();
+
+    return arr.flat()
+
 };
