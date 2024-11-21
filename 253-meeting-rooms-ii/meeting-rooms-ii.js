ee708101d62317860c3db7c8c38f6fccac4619c0
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-20 21:48:37 -0800

    Time: 23 ms (18.97%) | Memory: 55.5 MB (20.71%) - LeetSync

---
+++ 253-meeting-rooms-ii/meeting-rooms-ii.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+const minMeetingRooms = function (intervals) {
+	intervals.sort((a, b) => a[0] - b[0]);
+
+	const finish = new MinPriorityQueue();
+	let rooms = 0;
+
+	for (let i = 0; i < intervals.length; i++) {
+		const interval = intervals[i];
+
+		if (!finish.size()) rooms++;
+		else {
+			const front = finish.front().element;
+
+			if (front <= interval[0]) finish.dequeue();
+			else rooms++;
+		}
+
+		finish.enqueue(interval[1]);
+	}
+
+	return rooms;
+};
