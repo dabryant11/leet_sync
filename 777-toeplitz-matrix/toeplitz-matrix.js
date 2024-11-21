0e08bd5afae7354aa9b8a166216d1448e0055893
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-20 21:44:07 -0800

    Time: 0 ms (100.00%) | Memory: 50.5 MB (89.89%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.js
+++ 777-toeplitz-matrix/toeplitz-matrix.js
@@ -3,11 +3,10 @@
  * @return {boolean}
  */
 var isToeplitzMatrix = function(matrix) {
-    for (let i = 1; i < matrix.length; i++) {
-        for (let j = 0; j < matrix[0].length; j++) {
-            if (matrix[i-1][j - 1] !== undefined && matrix[i][j] !== matrix[i-1][j - 1]) return false;
+    for (let i =1; i<matrix.length;i++){
+        for (let j =0; j<matrix[0].length;j++){
+            if(matrix[i -1][j-1] !== undefined && matrix[i -1][j-1] !== matrix[i][j]) return false
         }
     }
-    return true;
-
+    return true
 };
