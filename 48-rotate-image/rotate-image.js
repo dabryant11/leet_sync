bea12e696a334bcfb2f1539a9112791547546fae
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-21 21:32:58 -0800

    Time: 0 ms (100.00%) | Memory: 49.1 MB (71.84%) - LeetSync

--- 48-rotate-image/rotate-image.js
+++ 48-rotate-image/rotate-image.js
@@ -6,7 +6,8 @@
  var rotate = function(matrix) {
     let n = matrix.length, depth = ~~(n / 2)
     for (let i = 0; i < depth; i++) {
-        let len = n - 2 * i - 1, opp = n - 1 - i
+        let len = n - 2 * i - 1
+        let opp = n - 1 - i
         for (let j = 0; j < len; j++) {
             let temp = matrix[i][i+j]
             matrix[i][i+j] = matrix[opp-j][i]
@@ -17,18 +18,3 @@
     }
     return matrix
 };
-
-// var rotate = function(matrix) {
-//     let rows = matrix.length
-//     let cols = matrix[0].length
-
-//     for(let i = 0; i<rows.length; i++){
-//         // let y = rows.length -1
-//         for(let j = 0; j<rows.length; j++){
-//         // let z = cols.length -1
-//             [ matrix[i][j],matrix[i][j] ] = [ matrix[i][j],matrix[i][j] ]
-//         }
-//     }
-
-//     return matrix
-// };
