4113d747b8491eab60304f273d59786323571a12
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-21 18:22:08 -0800

    Time: 64 ms (53.39%) | Memory: 51.5 MB (82.15%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.js
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.js
@@ -12,27 +12,26 @@
  * @return {number[]}
  */
 var distanceK = function(root, target, k) {
-    function addParents(root,parent = null){
-        if(!root) return
-        root.parent = parent
-        addParents(root.left,root)
-        addParents(root.right,root)
+    function addParents(node,parent=null){
+        if(!node)return
+        node.parent = parent
+        addParents(node.left,node)
+        addParents(node.right,node)
     }
-
-    function getDepth(node,depth = 0){
+    function getDepth(node,depth=0){
         if(!node || depth > k) return
         if(visited.has(node)) return
-        if(depth == k) res.push(node.val)
-        visited.add(node)
+        if(depth === k) res.push(node.val)
+        visited.add(node) 
 
         getDepth(node.left,depth+1)
         getDepth(node.right,depth+1)
         getDepth(node.parent,depth+1)
     }
 
+    addParents(root)
     let res = []
     let visited = new Set()
-    addParents(root)
     getDepth(target)
 
     return res
