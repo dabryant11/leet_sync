2cdfba5df845b5e77a50de965ce2a09b3dd5619c
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-23 22:28:54 -0800

    Time: 0 ms (100.00%) | Memory: 48.7 MB (71.55%) - LeetSync

--- 163-missing-ranges/missing-ranges.js
+++ 163-missing-ranges/missing-ranges.js
@@ -6,16 +6,14 @@
  */
 var findMissingRanges = function(nums, lower, upper) {
     let ans = []
-     nums = [lower - 1, ...nums, upper +1]
+    nums = [lower -1, ...nums,upper +1]
 
-    for ( let i=1; i< nums.length ;i++){
-        let start = nums[i-1] + 1
+    for ( let i =1 ;i<nums.length;i++){
+        let start = nums[i-1] +1
         let end = nums[i]-1
 
-        if(start == end) ans.push([start,end])
-        else if (start < end){
-            ans.push([start,end])
-        }
+        if(start === end) ans.push([start,end])
+        else if(start < end) ans.push([start,end])
     }
     return ans
 };
