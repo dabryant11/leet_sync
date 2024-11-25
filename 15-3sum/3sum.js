192d53aaef29945c88d968356b469b6f628022e6
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-11-24 22:34:25 -0800

    Time: 33 ms (83.82%) | Memory: 65.2 MB (80.71%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -5,23 +5,21 @@
 var threeSum = function(nums) {
     let sorted = nums.sort((a,b) => a-b)
     let ans = []
+    for ( let i =0;i<sorted.length-2;i++){
+        if (i>0 && sorted[i] === sorted[i-1]) continue
 
-    for ( let i = 0;i<sorted.length -2;i++){
-        if(i > 0 && sorted[i] == sorted [i-1]) continue
-
-        let left = i + 1
+        let left = i +1
         let right = sorted.length -1
-
         while ( left < right){
             let sum = sorted[i] + sorted[left] + sorted[right]
-            if (sum == 0){
+            if ( sum === 0){
                 ans.push([sorted[i],sorted[left],sorted[right]])
-                while (sorted[left] == sorted[left +1]) left ++
-                while (sorted[right] == sorted[right -1]) right ++
-                left ++ 
+                while ( sorted[right] == sorted[right-1]) right --
+                while ( sorted[left] == sorted[left+1]) left ++
+                left ++
                 right --
-            } else if ( sum > 0) right --
-            else  left ++
+            } else if (sum > 0) right --
+            else left ++
         }
     }
     return ans
