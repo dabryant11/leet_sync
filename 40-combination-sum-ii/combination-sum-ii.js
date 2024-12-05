dd35272bfbff983721fe5718f29eae391d282337
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-12-05 14:57:37 -0800

    Time: 4 ms (67.08%) | Memory: 52.7 MB (61.67%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.js
+++ 40-combination-sum-ii/combination-sum-ii.js
@@ -6,22 +6,16 @@
 var combinationSum2 = function(candidates, target) {
     candidates.sort((a,b) => a-b)
     let res = []
-
     function dfs(target,start,combo){
-        if(target < 0) return
-        if(target == 0) {
+        if(target<0) return
+        if(target === 0) {
             res.push(combo.slice())
             return
         }
-
-        for(let i= start;i<candidates.length;i++){
-            if(i > start && candidates[i] === candidates[i-1]) continue
-            if(candidates[i] > target ) break
-
-            
-            // dfs(target, i, [])
-            dfs(target - candidates[i], i + 1, combo.concat(candidates[i]));
-
+        for(let i = start;i< candidates.length;i++){
+            if(i> start && candidates[i] === candidates[i-1]) continue
+            if(candidates[i] > target) break
+            dfs(target - candidates[i],i+1,combo.concat(candidates[i]))
         }
     }
     dfs(target,0,[])
