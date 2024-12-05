fa80f9ebc7c4fc6c08bde20511236d86efd7c4e5
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-12-05 14:07:56 -0800

    Time: 11 ms (31.31%) | Memory: 57.3 MB (35.98%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.js
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -6,20 +6,20 @@ var validPalindrome = function(s) {
     let left = 0
     let right = s.length -1
 
+    function helper(left,right){
+        while ( left < right){
+            if (s[left] !== s[right]) return false
+            left ++
+            right --
+        }    
+        return true
+    }
     while ( left < right){
-        if ( s[right] !== s[left]) return isPalindrome(s,left +1,right) || isPalindrome(s,left,right -1)
+        if (s[left] !== s[right]){
+            return helper(left +1, right) || helper(left, right -1)
+        }
         left ++
         right --
     }
     return true
 };
-
-function isPalindrome(s,left,right){
-    while (left < right){
-        if(s[left] !== s[right]) return false
-        left ++
-        right --
-    }
-
-    return true
-}
