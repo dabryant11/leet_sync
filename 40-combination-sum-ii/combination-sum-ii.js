435f2de4464263724de4d6e6ff7440e98bbdc995
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-12-05 14:51:56 -0800

    Time: 4 ms (67.08%) | Memory: 52.3 MB (82.13%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.js
+++ 40-combination-sum-ii/combination-sum-ii.js
@@ -4,33 +4,26 @@
  * @return {number[][]}
  */
 var combinationSum2 = function(candidates, target) {
-    candidates.sort((a, b) => a - b);
-    const res = [];
+    candidates.sort((a,b) => a-b)
+    let res = []
 
-    function dfs(target, start, comb) {
-        if (target < 0) {
-            return;
+    function dfs(target,start,combo){
+        if(target < 0) return
+        if(target == 0) {
+            res.push(combo.slice())
+            return
         }
 
-        if (target === 0) {
-            res.push(comb.slice());
-            return;
-        }
-        
-        for (let i = start; i < candidates.length; i++) {
+        for(let i= start;i<candidates.length;i++){
+            if(i > start && candidates[i] === candidates[i-1]) continue
+            if(candidates[i] > target ) break
 
-            if (i > start && candidates[i] === candidates[i-1]) {
-                continue;
-            }
+            
+            // dfs(target, i, [])
+            dfs(target - candidates[i], i + 1, combo.concat(candidates[i]));
 
-            if (candidates[i] > target) {
-                break;
-            }
-
-            dfs(target - candidates[i], i + 1, comb.concat(candidates[i]));
         }
     }
-
-    dfs(target, 0, []);
-    return res;
+    dfs(target,0,[])
+    return res
 };
