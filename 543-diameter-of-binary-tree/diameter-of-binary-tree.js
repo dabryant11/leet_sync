24fdf8f79a8ecd76393e9af8dfd35707962b3c4f
darien bryant <74263550+dabryant11@users.noreply.github.com>
2024-12-06 10:07:08 -0800

    Time: 1 ms (87.05%) | Memory: 56.8 MB (44.98%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.js
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.js
@@ -13,13 +13,12 @@
 var diameterOfBinaryTree = function(root) {
     let longestPath = 0
     function dfs(root){
-        if(!root) return 0
+        if (!root) return 0
         let leftPath = dfs(root.left)
         let rightPath = dfs(root.right)
-
-        longestPath = Math.max(longestPath, rightPath + leftPath)
-        return Math.max(rightPath , leftPath) +1
+        longestPath = Math.max(longestPath,rightPath + leftPath)
+        return Math.max(leftPath, rightPath) +1
     }
     dfs(root)
-    return longestPath
+    return longestPath 
 };
