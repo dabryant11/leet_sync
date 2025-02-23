37927527b6007f1ff7a3af6afefad903d045054b
darien bryant <74263550+dabryant11@users.noreply.github.com>
2025-02-22 17:21:04 -0800

    Time: 68 ms (7.23%) | Memory: 60.6 MB (5.20%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.js
@@ -1,4 +1,3 @@
-
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
@@ -7,10 +6,9 @@
 var findDifference = function(nums1, nums2) {
     let ans = [[],[]]
     let longest = Math.max(nums1.length,nums2.length)
-
-    for ( let i =0;i<longest;i++){
-        if( i < nums1.length && !nums2.includes(nums1[i]) && !ans[0].includes(nums1[i])) ans[0].push(nums1[i])
-        if( i < nums2.length && !nums1.includes(nums2[i]) && !ans[1].includes(nums2[i])) ans[1].push(nums2[i])
+    for(let i =0;i<longest;i++){
+        if(i<nums1.length && !nums2.includes(nums1[i]) && !ans[0].includes(nums1[i])) ans[0].push(nums1[i])
+        if(i<nums2.length && !nums1.includes(nums2[i]) && !ans[1].includes(nums2[i])) ans[1].push(nums2[i])
     }
-    return ans 
+    return ans
 };
