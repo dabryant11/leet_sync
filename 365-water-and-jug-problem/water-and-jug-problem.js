e91eae30005a04b4024a7a812fcb3116152376f1
darien bryant <74263550+dabryant11@users.noreply.github.com>
2025-03-12 12:11:15 -0700

    Time: 0 ms (100.00%) | Memory: 53.9 MB (25.35%) - LeetSync

--- 365-water-and-jug-problem/water-and-jug-problem.js
+++ 365-water-and-jug-problem/water-and-jug-problem.js
@@ -5,22 +5,15 @@
  * @return {boolean}
  */
 var canMeasureWater = function(x, y, target) {
-   
-
-    if (target > x + y) return false; // if the target exceeds the total capacity of both jugs, it's impossible
-    if (target === 0) return true; // target 0 is always achievable (by not filling the jugs)
-    
-    // Function to calculate GCD
-    function gcd(a, b) {
-        while (b !== 0) {
-            let temp = b;
-            b = a % b;
-            a = temp;
+    if ( target === 0) return true
+    if ( target > x + y) return false
+    function gcd(a,b){
+        while(b!==0){
+            let temp = b
+            b = a%b
+            a = temp
         }
-        return a;
+        return a
     }
-
-    // Check if target is a multiple of the GCD of x and y
-    return target % gcd(x, y) === 0;
-    
+    return target % gcd(x,y) === 0
 };
