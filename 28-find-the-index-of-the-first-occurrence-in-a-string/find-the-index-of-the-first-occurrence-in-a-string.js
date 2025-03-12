2aa404c4e5d333e6b4e3cafcc37e6f9dd5c2383f
darien bryant <74263550+dabryant11@users.noreply.github.com>
2025-03-11 20:16:41 -0700

    Time: 0 ms (100.00%) | Memory: 54.4 MB (9.96%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,13 +4,14 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
- let n = haystack.length
- let m = needle.length
+    let n = haystack.length
+    let m = needle.length
 
- for(let i = 0;i<=n-m;i++){
-    let j= 0 
-    while( j< m && haystack[i+j] ===  needle[j]) j++
-    if( j == m) return i
- }
- return -1
+    for ( let i =0;i<= n-m;i++){
+        let j = 0
+        while (j<m && haystack[i+j] === needle[j]) j++
+        if(j == m ) return i
+    }
+
+    return -1
 };
