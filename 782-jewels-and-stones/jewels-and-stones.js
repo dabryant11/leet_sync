9e4cfad3d63f30670dabfbda444bb5462c8c390e
darien bryant <74263550+dabryant11@users.noreply.github.com>
2025-03-11 19:37:50 -0700

    Time: 1 ms (54.29%) | Memory: 54.3 MB (13.48%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -4,15 +4,10 @@
  * @return {number}
  */
 var numJewelsInStones = function(jewels, stones) {
-    let map = {}
+    let jewelSet = new Set(jewels)
     let ans = 0
-
-    for ( let i =0;i<jewels.length;i++){
-        if(!map[jewels[i]]) map[jewels[i]] = 1
-    }
-
-    for ( let i =0;i<stones.length;i++){
-        if(map[stones[i]]) ans ++
+    for (let s of stones){
+        if (jewelSet.has(s)) ans ++
     }
     return ans
 };
