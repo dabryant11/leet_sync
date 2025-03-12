480912152143d038a55cae05bde55cc126ed50ac
darien bryant <74263550+dabryant11@users.noreply.github.com>
2025-03-11 20:09:43 -0700

    Time: 1 ms (21.91%) | Memory: 54.1 MB (13.57%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.js
@@ -4,17 +4,13 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    
- let hay = haystack.length
- let need = needle.length;
+ let n = haystack.length
+ let m = needle.length
 
-    for (let i = 0; i <= hay - need; i++) {  // Only iterate until valid start positions
-        let j = 0;
-        while (j < need && haystack[i + j] === needle[j]) {  
-            j++;
-        }
-        if (j === need) return i;  // If full match found, return starting index
-    }
-
-    return -1; // Not found
+ for(let i = 0;i<=n-m;i++){
+    let j= 0 
+    while( j< m && haystack[i+j] ===  needle[j]) j++
+    if( j == m) return i
+ }
+ return -1
 };
